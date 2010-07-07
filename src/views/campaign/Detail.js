9c6a18ea15546172235fa0ce55fc8a918d9cbf47
prabhu c <prabhu.chinnamuniyandi@csscorp.com>
2010-07-07 21:04:54 +0530

    [#4179313]. Implemented campaign list,detail,edit views

---
+++ src/views/campaign/Detail.js
@@ -0,0 +1,53 @@
+﻿/// <reference path="../../../../ext/ext-core-debug.js"/>
+/// <reference path="../../../../Simplate.js"/>
+/// <reference path="../../../../sdata/SDataSingleResourceRequest.js"/>
+/// <reference path="../../../../sdata/SDataService.js"/>
+/// <reference path="../../../../platform/View.js"/>
+/// <reference path="../../../../platform/Detail.js"/>
+/// <reference path="../../Format.js"/>
+
+Ext.namespace("Mobile.SalesLogix.Campaign");
+
+Mobile.SalesLogix.Campaign.Detail = Ext.extend(Sage.Platform.Mobile.Detail, {       
+    constructor: function(o) {
+        Mobile.SalesLogix.Campaign.Detail.superclass.constructor.call(this);        
+        
+        Ext.apply(this, o, {
+            id: 'campaign_detail',
+            title: 'Campaign',
+            editor: 'campaign_edit',
+            resourceKind: 'campaigns'
+        });
+
+        this.layout = [
+            {name: 'CampaignName', label: 'campaignname'},
+            {name: 'CampaignCode', label: 'campaigncode'},
+            {name: 'StartDate', label: 'start date', renderer: Mobile.SalesLogix.Format.date},
+            {name: 'AccountManager.UserInfo', label: 'acct mgr', tpl: Mobile.SalesLogix.Template.nameLF},
+            {name: 'CreateUser', label: 'create user'},
+            {name: 'CreateDate', label: 'create date', renderer: Mobile.SalesLogix.Format.date},
+          ];
+    },
+    init: function() {     
+        Mobile.SalesLogix.Campaign.Detail.superclass.init.call(this);   
+    },
+    createRequest: function() {
+        var request = Mobile.SalesLogix.Campaign.Detail.superclass.createRequest.call(this);
+        
+        request                     
+            .setQueryArgs({
+                'include': 'Address,AccountManager/UserInfo',
+                'select': [
+                    'CampaignName',
+                    'CampaignCode',
+                    'StartDate',
+                    'AccountManager/UserInfo/FirstName',
+                    'AccountManager/UserInfo/LastName',
+                    'CreateUser',
+                    'CreateDate'
+                  ].join(',')                  
+            });     
+        
+        return request;                   
+    } 
+});
---
+++ src/views/campaign/Edit.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="../../../../ext/ext-core-debug.js"/>
+/// <reference path="../../../../Simplate.js"/>
+/// <reference path="../../../../sdata/SDataSingleResourceRequest.js"/>
+/// <reference path="../../../../sdata/SDataService.js"/>
+/// <reference path="../../../../platform/View.js"/>
+/// <reference path="../../../../platform/Detail.js"/>
+/// <reference path="../../Format.js"/>
+
+Ext.namespace("Mobile.SalesLogix.Campaign");
+
+Mobile.SalesLogix.Campaign.Edit = Ext.extend(Sage.Platform.Mobile.Edit, {       
+    constructor: function(o) {
+        Mobile.SalesLogix.Campaign.Edit.superclass.constructor.call(this);        
+        
+        Ext.apply(this, o, {
+            id: 'campaign_edit',
+            title: 'Campaign',
+            resourceKind: 'campaigns'
+        });
+
+        this.layout = [
+            {name: 'CampaignName', label: 'campaignname', type: 'text'},
+            {name: 'CampaignCode', label: 'campaigncode', type: 'text'},
+            {name: 'StartDate', label: 'start date', type: 'text'},
+            //{name: 'AccountManager', label: 'accountmanager', type: 'text'},
+          ];
+    },
+    init: function() {     
+        Mobile.SalesLogix.Campaign.Edit.superclass.init.call(this);   
+    },
+    createRequest: function() {
+        return new Sage.SData.Client.SDataSingleResourceRequest(this.getService())            
+            .setResourceKind(this.resourceKind)
+            .setQueryArgs({
+                'select': [
+                    'CampaignName',
+                    'CampaignCode',
+                    'StartDate'//,
+                    //'AccountManager'
+                   ]
+            })
+            .setResourceSelector(String.format("'{0}'", this.entry['$key']));
+    }
+});
