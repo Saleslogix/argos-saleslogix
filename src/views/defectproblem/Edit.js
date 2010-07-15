ffabe4bd258417b7922d310d069ff8d61d370964
prabhu c <prabhu.chinnamuniyandi@csscorp.com>
2010-07-15 20:24:33 +0530

    [#4230355]. Completed detail, edit views for Defect problem and solutions.

---
+++ src/views/defectproblem/Edit.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="../../../../ext/ext-core-debug.js"/>
+/// <reference path="../../../../Simplate.js"/>
+/// <reference path="../../../../sdata/SDataSingleResourceRequest.js"/>
+/// <reference path="../../../../sdata/SDataService.js"/>
+/// <reference path="../../../../platform/View.js"/>
+/// <reference path="../../../../platform/Detail.js"/>
+/// <reference path="../../Format.js"/>
+
+Ext.namespace("Mobile.SalesLogix.DefectProblem");
+
+Mobile.SalesLogix.DefectProblem.Edit = Ext.extend(Sage.Platform.Mobile.Edit, {       
+    constructor: function(o) {
+        Mobile.SalesLogix.DefectProblem.Edit.superclass.constructor.call(this);        
+        
+        Ext.apply(this, o, {
+            id: 'defectproblem_edit',
+            title: 'DefectProblem',
+            resourceKind: 'defectproblems'
+        });
+
+        this.layout = [
+            {name: 'Notes', label: 'notes', type: 'text'},
+           ];
+    },
+    init: function() {     
+        Mobile.SalesLogix.DefectProblem.Edit.superclass.init.call(this);   
+    },
+    createRequest: function() {
+        return new Sage.SData.Client.SDataSingleResourceRequest(this.getService())            
+            .setResourceKind(this.resourceKind)
+            .setQueryArgs({
+                'select': [
+                    'Notes'
+                    ]
+            })
+            .setResourceSelector(String.format("'{0}'", this.entry['$key']));
+    }
+});
---
+++ src/views/defectsolution/Edit.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="../../../../ext/ext-core-debug.js"/>
+/// <reference path="../../../../Simplate.js"/>
+/// <reference path="../../../../sdata/SDataSingleResourceRequest.js"/>
+/// <reference path="../../../../sdata/SDataService.js"/>
+/// <reference path="../../../../platform/View.js"/>
+/// <reference path="../../../../platform/Detail.js"/>
+/// <reference path="../../Format.js"/>
+
+Ext.namespace("Mobile.SalesLogix.DefectSolution");
+
+Mobile.SalesLogix.DefectSolution.Edit = Ext.extend(Sage.Platform.Mobile.Edit, {       
+    constructor: function(o) {
+        Mobile.SalesLogix.DefectSolution.Edit.superclass.constructor.call(this);        
+        
+        Ext.apply(this, o, {
+            id: 'defectsolution_edit',
+            title: 'DefectSolution',
+            resourceKind: 'defectsolutions'
+        });
+
+        this.layout = [
+            {name: 'Notes', label: 'notes', type: 'text'},
+           ];
+    },
+    init: function() {     
+        Mobile.SalesLogix.DefectSolution.Edit.superclass.init.call(this);   
+    },
+    createRequest: function() {
+        return new Sage.SData.Client.SDataSingleResourceRequest(this.getService())            
+            .setResourceKind(this.resourceKind)
+            .setQueryArgs({
+                'select': [
+                    'Notes'
+                    ]
+            })
+            .setResourceSelector(String.format("'{0}'", this.entry['$key']));
+    }
+});
