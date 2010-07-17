00d697f4a070b92a1714496255ee0a030e31b905
mmorton <mmorton@icanhascode.com>
2010-07-16 17:32:31 -0700

    added a method for rendering large blocks of linked text

--- src/views/defect/Detail.js
+++ src/views/defect/Detail.js
@@ -9,7 +9,15 @@
 
 Ext.namespace("Mobile.SalesLogix.Defect");
 
-Mobile.SalesLogix.Defect.Detail = Ext.extend(Sage.Platform.Mobile.Detail, {       
+Mobile.SalesLogix.Defect.Detail = Ext.extend(Sage.Platform.Mobile.Detail, {      
+    textBlockTemplate: new Simplate([
+        '<div class="row defect-text-row">',
+        '<div class="defect-text-wrap">',
+        '<a href="#{%= $["view"] %}" m:context={%= $["context"] %}>{%= $["value"] %}</a>',        
+        '</div>',
+        '<div class="defect-text-more">more &gt;&gt;</div>',
+        '</div>'
+    ]),     
     constructor: function(o) {
         Mobile.SalesLogix.Defect.Detail.superclass.constructor.call(this);        
         
@@ -21,7 +29,7 @@ Mobile.SalesLogix.Defect.Detail = Ext.extend(Sage.Platform.Mobile.Detail, {
         });
 
         this.layout = [
-            {label: 'defect id', tpl: Mobile.SalesLogix.Template.alternatekeyPrefixSuffix},
+            {label: 'defect id', tpl: Mobile.SalesLogix.Template.alternateKeyPrefixSuffix},
             {name: 'PriorityCode', label: 'priority'},
             {name: 'SeverityCode', label: 'severity'},
             {name: 'Area', label: 'area'},
@@ -32,26 +40,43 @@ Mobile.SalesLogix.Defect.Detail = Ext.extend(Sage.Platform.Mobile.Detail, {
             {name: 'StatusCode', label: 'status'},
             {name: 'CreateUser', label: 'create user'},
             {name: 'CreateDate', label: 'create date', renderer: Mobile.SalesLogix.Format.date},
+            {options: {title: 'Problem'}, as: [
+                {
+                    cls: 'defect-text-limit',
+                    name: 'DefectSolution.Notes',
+                    wrap: this.textBlockTemplate,
+                    view: 'defectproblem_detail', key: 'DefectProblem.$key'
+                }
+            ]},
             {options: {title: 'Related Items', list: true}, as: [
                 {
                     view: 'defectproblem_related', 
                     where: this.formatRelatedQuery.createDelegate(this, ['CreateUser eq "{0}"'], true),
                     label: 'Problem',
                     icon: 'content/images/defect_detail_24x24.gif'
-                },
-               
+                }               
             ]} 
         ];
     },
     init: function() {     
         Mobile.SalesLogix.Defect.Detail.superclass.init.call(this);   
+
+        Ext.EventManager.on(window, 'resize', this.onResize, this, {buffer: 250});
+    },    
+    onResize: function() {  
+        this.el.select('.defect-text-row').each(function(el) {
+            if (el.child('.defect-text-wrap').getHeight(true) < el.child('.defect-text-wrap a').getHeight())
+                el.child('.defect-text-more').show()
+            else
+                el.child('.defect-text-more').hide();
+        });             
     },
     createRequest: function() {
         var request = Mobile.SalesLogix.Defect.Detail.superclass.createRequest.call(this);
         
         request                     
             .setQueryArgs({
-                'include': 'AssignedTo',
+                'include': 'AssignedTo,DefectProblem,DefectSolution',
                 'select': [
                     'AlternateKeyPrefix',
                     'AlternateKeySuffix',
@@ -62,6 +87,8 @@ Mobile.SalesLogix.Defect.Detail = Ext.extend(Sage.Platform.Mobile.Detail, {
                     'Subject',
                     'RecordedDate',
                     'AssignedTo/OwnerDescription',
+                    'DefectProblem/Notes',
+                    'DefectSolution/Notes',
                     'StatusCode',
                     'CreateUser',
                     'CreateDate'
@@ -69,5 +96,10 @@ Mobile.SalesLogix.Defect.Detail = Ext.extend(Sage.Platform.Mobile.Detail, {
             });     
         
         return request;                   
-    } 
+    },
+    processEntry: function(entry) {
+        Mobile.SalesLogix.Defect.Detail.superclass.processEntry.call(this, entry);
+
+        this.onResize();    
+    }
 });
