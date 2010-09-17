41a0662299475633346ea14d997211f6999fed92
Peroli Sivaprakasam <peroli.sivaprakasam@csscorp.com>
2010-09-17 18:19:56 +0530

    [#4588179] Add Account/Contact screen.   - Changed fields to appropriate picklists.   - Augmented base implementation of "createEntryForInsert" to fit our needs.

--- src/views/account/AddAccountContact.js
+++ src/views/account/AddAccountContact.js
@@ -14,15 +14,18 @@ Mobile.SalesLogix.Account.AddAccountContact = Ext.extend(Sage.Platform.Mobile.Ed
     detailsContactText: 'Contact Info',
     detailsAccountText: 'Account Info',
     accountText: 'Account',
-    firstNameText: 'firstName',
-    lastNameText: 'lastName',
-    phoneText: 'phone',
-    webText: 'Web',
-    workText: 'workPhone',
+    accountNameText: 'accountname',
+    firstNameText: 'first',
+    lastNameText: 'last',
+    emailText: 'e-mail',
+    webText: 'web',
+    workText: 'work phone',
+    titleText: 'title',
+    homePhoneText: 'home phone',
     mobileText: 'mobile',
     faxText: 'fax',
     typeText: 'type',
-    subTypeText: 'subtype',
+    subTypeText: 'sub-type',
     industryText: 'industry',
     description: 'description',
     statusText: 'status',
@@ -37,48 +40,66 @@ Mobile.SalesLogix.Account.AddAccountContact = Ext.extend(Sage.Platform.Mobile.Ed
         });
 
         this.layout = [
-            {name: 'AccountManager.UserInfo.FirstName', label: this.firstNameText, type: 'text'},
-            {name: 'AccountManager.UserInfo.LastName', label: this.lastNameText, type: 'text'},
-            {name: 'MainPhone', label: this.phoneText, type: 'text'},
-            {name: 'WebAddress', label: this.webText, type: 'text'},
+            {name: 'Contact.FirstName', label: this.firstNameText, type: 'text'},
+            {name: 'Contact.LastName', label: this.lastNameText, type: 'text'},
+            {name: 'Account.AccountName', label: this.accountNameText, type: 'text'},
+            {name: 'Contact.Email', label: this.emailText, type: 'text'},
+            {name: 'Contact.WebAddress', label: this.webText, type: 'text'},
+            {name: 'Contact.WorkPhone', label: this.workText, type: 'phone'},
             {options: {title: this.detailsContactText}, as: [
-                {name: 'WorkPhone', label: this.workText, type: 'text'},
-                {name: 'Mobile', label: this.mobileText, type: 'text'},
-                {name: 'Fax', label: this.faxText, type: 'text'}
+                {name: 'Contact.Title', label: this.titleText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "Title"', title: 'Title'},
+                {name: 'Contact.HomePhone', label: this.homePhoneText, type: 'phone'},
+                {name: 'Contact.Mobile', label: this.mobileText, type: 'phone'},
+                {name: 'Contact.Fax', label: this.faxText, type: 'phone'}
             ]},
             {options: {title: this.detailsAccountText}, as: [
-                {name: 'Fax', label: this.faxText, type: 'text'},
-                {name: 'Type', label: this.typeText, type: 'text'},
-                {name: 'SubType', label: this.subTypeText, type: 'text'},
-                {name: 'Status', label: this.statusText, type: 'text'},
-                {name: 'Industry', label: this.industryText, type: 'text'},
-                {name: 'BusinessDescription', label: this.description, type: 'text'}
+                {name: 'Account.Fax', label: this.faxText, type: 'phone'},
+                {name: 'AccountType', label: this.typeText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "Account Type"', title: 'Account Type', orderBy: 'sort asc'},
+                {name: 'Account.SubType', label: this.subTypeText, type: 'pickup', view: 'pick_list', resourcePredicate: new Simplate(['name eq "Account {%= AccountType %}"']), title: 'Account SubType', dependsOn: 'AccountType', errMsg: 'A "Type" is required for "SubType"', orderBy: 'sort asc'},
+                {name: 'Account.Status', label: this.statusText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "Account Status"', title: 'Account Status', orderBy: 'sort asc'},
+                {name: 'Account.Industry', label: this.industryText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "Industry"', title: 'Industry', orderBy: 'sort asc'},
+                {name: 'Account.BusinessDescription', label: this.description, type: 'text'}
             ]}
         ]
     },
     init: function() {
         Mobile.SalesLogix.Account.AddAccountContact.superclass.init.call(this);
     },
+    createEntryForInsert: function(values) {
+        var entry = Mobile.SalesLogix.Account.AddAccountContact.superclass.createEntryForInsert.call(this, values);
+
+        entry.Account.Type = entry.AccountType;
+        entry.Contact.AccountName = entry.Account.AccountName;
+        entry.Contacts = {'$resources': []};
+        entry.Contacts.$resources[0] = entry.Contact;
+        delete entry.AccountType;
+        delete entry.Contact;
+
+        return entry;
+    },
     createRequest: function() {
         return Mobile.SalesLogix.Account.AddAccountContact.superclass.createRequest.call(this)
             .setResourceKind(this.resourceKind)
             .setQueryArgs({
-                'include': 'Account,Address,AccountManager,AccountManager/UserInfo',
+                'include': 'Contact',
                 'select': [
                     'AccountName',
-                    'MainPhone',
-                    'WebAddress',
-                    'AccountManager/UserInfo/FirstName',
-                    'AccountManager/UserInfo/LastName',
-                    'WorkPhone',
-                    'Mobile',
                     'Fax',
                     'Type',
                     'SubType',
                     'Status',
                     'Industry',
                     'BusinessDescription',
-
+                    'Contact/FirstName',
+                    'Contact/LastName',
+                    'Contact/AccountName',
+                    'Contact/Email',
+                    'Contact/WebAddress',
+                    'Contact/WorkPhone',
+                    'Contact/Title',
+                    'Contact/HomePhone',
+                    'Contact/Mobile',
+                    'Contact/Fax'
                 ]
             });
     }
