4e0f80cd352e01c2f7c689a9ee726a78ea6b00a3
Peroli Sivaprakasam <peroli.sivaprakasam@csscorp.com>
2010-09-27 15:24:17 +0530

    Fixed field name dependence of Address view.

--- src/views/AddressEdit.js
+++ src/views/AddressEdit.js
@@ -19,17 +19,17 @@ Mobile.SalesLogix.AddressEdit = Ext.extend(Sage.Platform.Mobile.Edit, {
         });
 
         this.layout = [
-            {name: 'Address.IsPrimary', label: this.isPrimaryText, type: 'boolean'},
-            {name: 'Address.IsMailing', label: this.isMailingText, type: 'boolean'},
-            {name: 'Address.Address1', label: this.address1Text, type: 'text'},
-            {name: 'Address.Address2', label: this.address2Text, type: 'text'},
-            {name: 'Address.Address3', label: this.address3Text, type: 'text'},
-            {name: 'Address.City', label: this.cityText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "City"', title: 'City'},
-            {name: 'Address.State', label: this.stateText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "State"', title: 'State'},
-            {name: 'Address.PostalCode', label: this.postalCodeText, type: 'text'},
-            {name: 'Address.Country', label: this.countryText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "Country"', title: 'Country'},
-            {name: 'Address.Description', type: 'hidden'},
-            {name: 'Address.EntityId', type: 'hidden'}
+            {name: 'IsPrimary', label: this.isPrimaryText, type: 'boolean'},
+            {name: 'IsMailing', label: this.isMailingText, type: 'boolean'},
+            {name: 'Address1', label: this.address1Text, type: 'text'},
+            {name: 'Address2', label: this.address2Text, type: 'text'},
+            {name: 'Address3', label: this.address3Text, type: 'text'},
+            {name: 'City', label: this.cityText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "City"', title: 'City'},
+            {name: 'State', label: this.stateText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "State"', title: 'State'},
+            {name: 'PostalCode', label: this.postalCodeText, type: 'text'},
+            {name: 'Country', label: this.countryText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "Country"', title: 'Country'},
+            {name: 'Description', type: 'hidden'},
+            {name: 'EntityId', type: 'hidden'}
         ];
     }
 });
