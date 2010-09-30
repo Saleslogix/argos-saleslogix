eb90d342928555eac626206b8d669b098679bf3a
mmorton <mmorton@icanhascode.com>
2010-09-29 22:30:25 -0700

    continued refactoring of edit views and fields.

--- src/views/contact/Edit.js
+++ src/views/contact/Edit.js
@@ -18,41 +18,35 @@ Ext.namespace("Mobile.SalesLogix.Contact");
         emailText: 'email',
         webText: 'web',
         acctMgrText: 'acct mgr',
-        acctnameText: 'account',
-        hometext: 'home phone',
+        accountNameText: 'account',
+        homePhoneText: 'home phone',
         faxText: 'fax',
         addressText: 'address',
-        titlText: 'title',
-        contactownerText: 'owner',
-        constructor: function(o) {
-            Mobile.SalesLogix.Contact.Edit.superclass.constructor.call(this);
-
-            Ext.apply(this, o, {
-                id: 'contact_edit',
-                title: this.titleText,
-                resourceKind: 'contacts',
-                entityName: 'Contact'
-            });
-
-            this.layout = [
-                {name: 'FirstName', label: this.firstNameText, type: 'text', validator: Mobile.SalesLogix.Validator.hasText},
-                {name: 'LastName', label: this.lastNameText, type: 'text', validator: Mobile.SalesLogix.Validator.hasText},
-                {name: 'Account', label: this.acctnameText, type: 'lookup', view: 'acc_list', textProperty: 'AccountName', forceValue: true},
-                {name: 'WebAddress', label: this.webText, type: 'text'},
-                {name: 'WorkPhone', label: this.workText, type: 'phone', validator: Mobile.SalesLogix.Validator.isPhoneNumber, validationTrigger: 'keyup'},
-                {name: 'Email', label: this.emailText, type: 'text'},
-                {name: 'Title', label: this.titlText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "Title"', title: 'Title'},
-                {name: 'Address', label: this.addressText, view: 'address_edit', type: 'address', resourceKind: 'contacts', title: 'Address', renderer: function(value){return Mobile.SalesLogix.Format.address(value, true)}},
-                {name: 'HomePhone', label: this.hometext, type: 'phone', validator: Mobile.SalesLogix.Validator.isPhoneNumber, validationTrigger: 'keyup'},
-                {name: 'Mobile', label: this.mobileText, type: 'phone', validator: Mobile.SalesLogix.Validator.isPhoneNumber, validationTrigger: 'keyup'},
-                {name: 'Fax', label: this.faxText, type: 'phone', validator: Mobile.SalesLogix.Validator.isPhoneNumber, validationTrigger: 'keyup'},
-                {name: 'AccountManager', label: this.acctMgrText, type: 'lookup', view: 'user_list', keyProperty: '$key', textProperty: 'UserInfo', textTemplate: Mobile.SalesLogix.Template.nameLF},
-                {name: 'Owner', label: this.contactownerText, type: 'lookup', view: 'owner_list', keyProperty: '$key', textProperty: 'OwnerDescription'}
-            ];
-        },
-        init: function() {
-            Mobile.SalesLogix.Contact.Edit.superclass.init.call(this);
-        },
+        contactTitleText: 'title',
+        ownerText: 'owner',
+        resourceKind: 'contacts',
+        entityName: 'Contact',
+        querySelect: [
+            'Account/AccountName',
+            'NameLF',
+            'FirstName',
+            'LastName',
+            'AccountName',
+            'WorkPhone',
+            'Mobile',
+            'Email',
+            'Address/*',
+            'WebAddress',
+            'AccountManager/UserInfo/FirstName',
+            'AccountManager/UserInfo/LastName',
+            'Owner/OwnerDescription',
+            'CreateDate',
+            'CreateUser',
+            'Title',
+            'HomePhone',
+            'Mobile',
+            'Fax'
+        ],
         getValues: function() {
             var U = Sage.Platform.Mobile.Utility,
                 values = Mobile.SalesLogix.Contact.Edit.superclass.getValues.apply(this, arguments),
@@ -74,30 +68,29 @@ Ext.namespace("Mobile.SalesLogix.Contact");
                 this.fields['Account'].setValue(relatedAccount.entry);
             }
         },
+        createLayout: function() {
+            return this.layout || (this.layout = [
+                {name: 'FirstName', label: this.firstNameText, type: 'text', validator: Mobile.SalesLogix.Validator.hasText},
+                {name: 'LastName', label: this.lastNameText, type: 'text', validator: Mobile.SalesLogix.Validator.hasText},
+                {name: 'Account', label: this.accountNameText, type: 'lookup', view: 'acc_list', textProperty: 'AccountName', forceValue: true},
+                {name: 'WebAddress', label: this.webText, type: 'text'},
+                {name: 'WorkPhone', label: this.workText, type: 'phone', validator: Mobile.SalesLogix.Validator.isPhoneNumber, validationTrigger: 'keyup'},
+                {name: 'Email', label: this.emailText, type: 'text'},
+                {name: 'Title', label: this.contactTitleText, type: 'pickup', view: 'pick_list', resourcePredicate: 'name eq "Title"', title: 'Title'},
+                {name: 'Address', label: this.addressText, view: 'address_edit', type: 'address', resourceKind: 'contacts', title: 'Address', renderer: function(value){return Mobile.SalesLogix.Format.address(value, true)}},
+                {name: 'HomePhone', label: this.homePhoneText, type: 'phone', validator: Mobile.SalesLogix.Validator.isPhoneNumber, validationTrigger: 'keyup'},
+                {name: 'Mobile', label: this.mobileText, type: 'phone', validator: Mobile.SalesLogix.Validator.isPhoneNumber, validationTrigger: 'keyup'},
+                {name: 'Fax', label: this.faxText, type: 'phone', validator: Mobile.SalesLogix.Validator.isPhoneNumber, validationTrigger: 'keyup'},
+                {name: 'AccountManager', label: this.acctMgrText, type: 'lookup', view: 'user_list', keyProperty: '$key', textProperty: 'UserInfo', textTemplate: Mobile.SalesLogix.Template.nameLF},
+                {name: 'Owner', label: this.ownerText, type: 'lookup', view: 'owner_list', keyProperty: '$key', textProperty: 'OwnerDescription'}
+            ]);  
+        },
         createRequest: function() {
             return Mobile.SalesLogix.Contact.Edit.superclass.createRequest.call(this)
                 .setResourceKind(this.resourceKind)
                .setQueryArgs({
                     'select': [
-                        'Account/AccountName',
-                        'NameLF',
-                        'FirstName',
-                        'LastName',
-                        'AccountName',
-                        'WorkPhone',
-                        'Mobile',
-                        'Email',
-                        'Address/*',
-                        'WebAddress',
-                        'AccountManager/UserInfo/FirstName',
-                        'AccountManager/UserInfo/LastName',
-                        'Owner/OwnerDescription',
-                        'CreateDate',
-                        'CreateUser',
-                        'Title',
-                        'HomePhone',
-                        'Mobile',
-                        'Fax'
+
                     ].join(',')
                 })
         }
