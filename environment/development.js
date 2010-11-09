f63ac92876ffc8ba4ef2ffd8053af04c72284a87
mmorton <mmorton@icanhascode.com>
2010-11-09 13:54:59 -0700

    cleaned up a couple of build and deployment issues.

--- environment/development.js
+++ environment/development.js
@@ -4,7 +4,7 @@
         virtualDirectory: 'sdata',
         applicationName: 'slx',
         contractName: 'dynamic',
-        port: 80,
+        port: window.location.port && window.location.port != 80 ? window.location.port : false,
         json: true,
         protocol: /https/i.test(window.location.protocol) ? 'https' : false
     }, { isDefault: true, offline: true });
