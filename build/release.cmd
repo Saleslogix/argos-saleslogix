92309a7c4c608414ebe10cea9c4b9566e28b8266
mmorton <mmorton@icanhascode.com>
2010-11-18 18:54:09 -0700

    build and manifest updates.

--- build/release.cmd
+++ build/release.cmd
@@ -12,4 +12,5 @@ mkdir deploy\app\content\css
 %JAVA_HOME%\bin\java -Dfile.encoding=UTF-8 -jar "../../argos-sdk/tools/JSBuilder/JSBuilder2.jar" -v -p "build/release.jsb2" -d "."
 if %errorlevel% neq 0 exit /b %errorlevel% 
 xcopy index.* .\deploy\app /Y /Q
+xcopy index-nocache.* .\deploy\app /Y /Q
 xcopy help.* .\deploy\app /Y /Q
