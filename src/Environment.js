2a10cd5efadf99b94f6da79f91be96f012868f12
mmorton <mmorton@icanhascode.com>
2010-12-03 16:10:39 -0700

    small tweak to call/email initiation.

--- src/Environment.js
+++ src/Environment.js
@@ -8,10 +8,14 @@ Ext.namespace("Mobile.SalesLogix");
 Mobile.SalesLogix.Environment = (function() {
     return {
         initiateCall: function(number) {
-            window.location.href = String.format("tel:{0}", number);
+            setTimeout(function() {
+                window.location.href = String.format("tel:{0}", number);
+            }, 0);
         },
         initiateEmail: function(email) {
-            window.location.href = String.format("mailto:{0}", email);
+            setTimeout(function() {
+                window.location.href = String.format("mailto:{0}", email);
+            }, 0);
         },
         showMapForAddress: function(address) {
             window.location.href = String.format("http://maps.google.com/maps?q={0}", address);
