c2c9bccc25d8f0df62e6f88da0df358c71521ec4
mmorton <mmorton@icanhascode.com>
2010-12-16 21:00:42 -0700

    [fixed #4218981] fixed an issue with the log to history view not showing up on iphone.

--- src/Environment.js
+++ src/Environment.js
@@ -6,21 +6,24 @@
 Ext.namespace("Mobile.SalesLogix");
 
 Mobile.SalesLogix.Environment = (function() {
+    // todo: open a new browser window for these when on a mobile device?
+    // on a mobile device, launching an external handler can impact a view transition, and cause issues, which the timeout takes care of.
+    // not the best way, perhaps a post-transition callback should be used for launching these? check transitioning, then queue if needed?
     return {
         initiateCall: function(number) {
             setTimeout(function() {
                 window.location.href = String.format("tel:{0}", number);
-            }, 0);
+            }, 50);
         },
         initiateEmail: function(email) {
             setTimeout(function() {
                 window.location.href = String.format("mailto:{0}", email);
-            }, 0);
+            }, 50);
         },
         showMapForAddress: function(address) {
             setTimeout(function() {
                 window.location.href = String.format("http://maps.google.com/maps?q={0}", address);
-            }, 0);
+            }, 50);
         }
     };
 })();
