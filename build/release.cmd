a7070bba97fa98fc45f790f823a0310970be24cd
mmorton <mmorton@icanhascode.com>
2011-01-21 15:09:35 -0700

    updated build scripts.

--- build/release.cmd
+++ build/release.cmd
@@ -4,18 +4,18 @@ if exist deploy (
 	rmdir deploy /S /Q
 )
 
-mkdir deploy\app\content\javascript
-mkdir deploy\app\content\images
-mkdir deploy\app\content\css
+mkdir deploy\content\javascript
+mkdir deploy\content\images
+mkdir deploy\content\css
 
 ..\..\argos-sdk\tools\JsBit\jsbit.exe -p "build\release.jsb2" -d "."
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-xcopy index.html .\deploy\app /Y /Q
-xcopy index-nocache.html .\deploy\app /Y /Q
-xcopy unsupported.html .\deploy\app /Y /Q
-xcopy help.html .\deploy\app /Y /Q
-xcopy index.manifest .\deploy\app /Y /Q
-xcopy template.manifest .\deploy\app /Y /Q
-xcopy index.manifest.ashx .\deploy\app /Y /Q
-xcopy web.config .\deploy\app /Y /Q
+xcopy index.html .\deploy /Y /Q
+xcopy index-nocache.html .\deploy /Y /Q
+xcopy unsupported.html .\deploy /Y /Q
+xcopy help.html .\deploy /Y /Q
+xcopy index.manifest .\deploy /Y /Q
+xcopy template.manifest .\deploy /Y /Q
+xcopy index.manifest.ashx .\deploy /Y /Q
+xcopy web.config .\deploy /Y /Q
