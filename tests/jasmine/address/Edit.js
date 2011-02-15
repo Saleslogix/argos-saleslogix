5320030b9087602fc2a151bc02f9390a942a7a48
Peroli Sivaprakasam <peroli.sivaprakasam@csscorp.com>
2011-02-15 19:57:06 +0530

    [#1-79329] Set up validator for descriptor field in address.

--- tests/jasmine/address/Edit.js
+++ tests/jasmine/address/Edit.js
@@ -1,6 +1,12 @@
 describe("Address", function() {
     ///TODO: Make this test simpler
     describe("Edit View", function() {
+        beforeEach(function(){
+            this.addMatchers({
+                toContainError: JASMINE_SLX_MATCHERS.toContainError
+            });
+        });
+
         /// Defects: #1-79128
         it("must use entityName from parent view for 'Description' picklist", function() {
             var addressFld1 = new Sage.Platform.Mobile.Controls.AddressField(
@@ -56,6 +62,31 @@ describe("Address", function() {
             );
             addressFld.navigateToEditView();
             expect(App.getView('address_edit').fields['Description'].picklist()).toEqual("Address Description (Contact)");
+        });
+
+        /// Defects: #1-79329
+        it("must validate address field's 'description' for 64 char length", function() {
+            var view = App.getView('address_edit'),
+                errors = view.validate();
+
+            view.fields['Description'].setValue('');
+            expect(errors).not.toBeFalsy();
+            expect(errors).toContainError({
+                'name': 'Description',
+                'message': "The field 'description' must have a value."
+            });
+
+            view.fields['Description'].setValue('Office');
+            errors = view.validate();
+            expect(errors).toBeFalsy();
+
+            view.fields['Description'].setValue('12345678901234567890123456789012345678901234567890123456789012345');
+            errors = view.validate();
+            expect(errors).not.toBeFalsy();
+            expect(errors).toContainError({
+                'name': 'Description',
+                'message': "The field 'description' value exceeds the allowed limit in length."
+            });
         });
 
     });
