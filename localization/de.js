718386f9774f83a110e1047356b7a3adda4a5222
Jeff Hershauer <jhershauer@gmail.com>
2011-08-23 14:19:35 -0700

    Renamed localization files to the generic language identifier.

---
+++ localization/de.js
@@ -0,0 +1,52 @@
+﻿Mobile.CultureInfo = {
+  "name": "de",
+  "englishName": "German",
+  "nativeName": "Deutsch",
+  "numberFormat": {
+    "currencyDecimalDigits": 2,
+    "currencyDecimalSeparator": ",",
+    "isReadOnly": true,
+    "currencyGroupSizes": [
+      3
+    ],
+    "numberGroupSizes": [
+      3
+    ],
+    "percentGroupSizes": [
+      3
+    ],
+    "currencyGroupSeparator": ".",
+    "currencySymbol": "€",
+    "naNSymbol": "n. def.",
+    "currencyNegativePattern": 8,
+    "numberNegativePattern": 1,
+    "percentPositivePattern": 1,
+    "percentNegativePattern": 1,
+    "negativeInfinitySymbol": "-unendlich",
+    "negativeSign": "-",
+    "numberDecimalDigits": 2,
+    "numberDecimalSeparator": ",",
+    "numberGroupSeparator": ".",
+    "currencyPositivePattern": 3,
+    "positiveInfinitySymbol": "+unendlich",
+    "positiveSign": "+",
+    "percentDecimalDigits": 2,
+    "percentDecimalSeparator": ",",
+    "percentGroupSeparator": ".",
+    "percentSymbol": "%",
+    "perMilleSymbol": "‰",
+    "nativeDigits": [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9"
+    ],
+    "digitSubstitution": 1
+  }
+};
