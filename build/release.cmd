a611da010f09e8ddbf9e7b1f48c6b75ac989a1f7
mmorton <mmorton@icanhascode.com>
2011-09-09 11:14:16 -0700

    added java command line example to build script; fixed a few build path issues in build file.

--- build/release.cmd
+++ build/release.cmd
@@ -10,7 +10,12 @@ mkdir deploy\content\javascript
 mkdir deploy\content\images
 mkdir deploy\content\css
 
+REM .NET Build Tool
 ..\..\argos-sdk\tools\JsBit\jsbit.exe -p "build\release.jsb2" -d "."
+
+REM Java Build Tool
+REM %JAVA_HOME%\bin\java -Dfile.encoding=UTF-8 -jar "../../argos-sdk/tools/JSBuilder/JSBuilder2.jar" -v -p "build/release.jsb2" -d "."
+
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 xcopy index.html .\deploy /Y /Q
