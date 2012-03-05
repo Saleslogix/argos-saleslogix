cae2ff5af997d64c0af4384602f9539c2fc465fc
Michael Morton <mmorton@icanhascode.com>
2012-03-05 13:31:27 -0700

    added en-GB localization.

---
+++ localization/saleslogix/en-GB.js
@@ -0,0 +1,57 @@
+﻿define('localization/saleslogix/en-GB', ['localization/en-GB', 'Mobile/SalesLogix/ApplicationModule'], function() {
+
+localize("Sage.Platform.Mobile.Controls.DateField", {
+  "dateFormatText": "dd/MM/yyyy"
+});
+
+localize("Mobile.SalesLogix.Activity.Complete", {
+  "completedFormatText": "d/M/yyyy h:mm tt",
+  "startingFormatText": "d/M/yyyy h:mm tt"
+});
+
+localize("Mobile.SalesLogix.Activity.Detail", {
+  "startDateFormatText": "d/M/yyyy h:mm:ss tt",
+  "timelessDateFormatText": "d/M/yyyy",
+  "alarmDateFormatText": "d/M/yyyy h:mm:ss tt"
+});
+
+localize("Mobile.SalesLogix.Activity.Edit", {
+  "startingFormatText": "d/M/yyyy h:mm tt"
+});
+
+localize("Mobile.SalesLogix.Activity.List", {
+  "startDateFormatText": "ddd d/M/yy",
+  "startTimeFormatText": "h:mm"
+});
+
+localize("Mobile.SalesLogix.Calendar.MonthView", {
+  "monthTitleFormatText": "MMMM yyyy",
+  "dayTitleFormatText": "ddd d MMM, yyyy",
+  "dayStartTimeFormatText": "h:mm"
+});
+
+localize("Mobile.SalesLogix.Calendar.UserActivityList", {
+  "dateHeaderFormatText": "dddd, dd/MM/yyyy",
+  "startTimeFormatText": "h:mm"
+});
+
+localize("Mobile.SalesLogix.Calendar.WeekView", {
+  "weekTitleFormatText": "d MMM, yyyy",
+  "dayHeaderLeftFormatText": "ddd",
+  "dayHeaderRightFormatText": "d MMM, yyyy",
+  "startTimeFormatText": "h:mm"
+});
+
+localize("Mobile.SalesLogix.History.Detail", {
+  "dateFormatText": "d/M/yyyy h:mm:ss tt"
+});
+
+localize("Mobile.SalesLogix.History.Edit", {
+  "startingFormatText": "d/M/yyyy h:mm tt"
+});
+
+localize("Mobile.SalesLogix.History.List", {
+  "hourMinuteFormatText": "h:mm",
+  "dateFormatText": "d/M/yy"
+});
+});
