e2952f03687be1d5fb8506eb2364d275845a7f4c
Jason Best <skeeterbug@gmail.com>
2013-03-04 11:04:57 -0700

    Adding selenium test for logging in. Basic test to get started, and to test jenkins integration with selenium/rspec.

---
+++ tests/selenium/canLogin_spec.rb
@@ -0,0 +1,52 @@
+require "selenium-webdriver"
+require "rspec"
+include RSpec::Expectations
+
+describe "CanLogin" do
+
+  before(:each) do
+    @driver = Selenium::WebDriver.for :firefox
+    @base_url = "http://localhost/"
+    @accept_next_alert = true
+    @driver.manage.timeouts.implicit_wait = 30
+    @verification_errors = []
+  end
+  
+  after(:each) do
+    @driver.quit
+    @verification_errors.should == []
+  end
+  
+  it "test_can_login" do
+    @driver.get(@base_url + "/slxmobile/")
+    @driver.find_element(:css, "input[name=\"username\"]").clear
+    @driver.find_element(:css, "input[name=\"username\"]").send_keys "admin"
+    @driver.find_element(:css, "button.button.actionButton").click
+    verify { element_present?(:id, "pageTitle").should be_true }
+  end
+  
+  def element_present?(how, what)
+    @driver.find_element(how, what)
+    true
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    false
+  end
+  
+  def verify(&blk)
+    yield
+  rescue ExpectationNotMetError => ex
+    @verification_errors << ex
+  end
+  
+  def close_alert_and_get_its_text(how, what)
+    alert = @driver.switch_to().alert()
+    if (@accept_next_alert) then
+      alert.accept()
+    else
+      alert.dismiss()
+    end
+    alert.text
+  ensure
+    @accept_next_alert = true
+  end
+end
