8c7c6d8e1faeb18fe8a3293dbfdb8e98291facbd
chenrie <cameron.henrie@sage.com>
2013-06-12 16:04:54 -0700

    Adding Web Url amd removing zoom button

--- src/Views/Attachment/ViewAttachment.js
+++ src/Views/Attachment/ViewAttachment.js
@@ -76,7 +76,7 @@ define('Mobile/SalesLogix/Views/Attachment/ViewAttachment', [
            '</div>',
            '<div class="attachment-viewer-area">',
                '<table><tr valign="middle"><td align="center">',
-                 '<image id="attachment-image"></image>',
+                 '<image id="attachment-image" border="1"></image>',
                  '</table></td></tr>',
            '</div>'
         ]),
@@ -119,34 +119,22 @@ define('Mobile/SalesLogix/Views/Attachment/ViewAttachment', [
             return this.tools;
         },
         createLayout: function() {
-            if (has('ios')) {
-                this.tools = {
-                    'tbar': [{
-                        id: 'zoomAttachmnet',
-                        icon: 'content/images/icons/Scale_24.png',
-                        action: '_zoomAttachment',
-                        title: 'zoom attachment'
-                    }]
-                };
-            }
+           // if (has('ios')) {
+          //      this.tools = {
+          //          'tbar': [{
+          //              id: 'zoomAttachmnet',
+          //              icon: 'content/images/icons/Scale_24.png',
+          //              action: '_zoomAttachment',
+          //              title: 'zoom attachment'
+          //          }]
+          //      };
+          //  }
             return this.tools || (this.tools = []);
         },
         _loadAttachmentView: function(entry) {
-            if (this._viewImageOnly()) {
-                this._loadAttachmentViewImageOnly(entry);
-            }
-            else {
-                if (has('ios')) {
-                    this._loadAttachmentViewIOS(entry);
-                }
-                else if (has('android')) {
-                    this._loadAttachmentViewAndroid(entry);
-                } else {
-                    this._loadAttachmentViewOther(entry);
-                }
-            }
+                this._loadAttachmentView(entry);
         },
-        _loadAttachmentViewImageOnly: function(entry) {
+        _loadAttachmentView: function(entry) {
             var data, am, isFile, url, viewNode, tpl, dl, description, attachmentid,fileType, self;
             am = new AttachmentManager();
             
@@ -168,25 +156,14 @@ define('Mobile/SalesLogix/Views/Attachment/ViewAttachment', [
             if (isFile) {
                fileType = Utility.getFileExtension(entry.fileName)
                if (this._isfileTypeImage(fileType)) {
-                    viewNode = domConstruct.place(this.attachmentViewImageTemplate.apply(data, this), this.attachmentViewerNode, 'last');
+                   viewNode = domConstruct.place(this.attachmentViewImageTemplate.apply(data, this), this.attachmentViewerNode, 'last');
+                   tpl = this.downloadingTemplate.apply(this);
+                   dl = domConstruct.place(tpl, this.attachmentViewerNode, 'first');
+                   domClass.add(this.domNode, 'list-loading');
                     self = this;
                     attachmentid = entry.$key;
-                   //Blob url
-                    //am.getAttachmentFile(attachmentid, 'blob', function(responseInfo) {
-                    //    var blob, url, a, blobURL, image;
-                    //    blob = responseInfo.response;
-                    //    url = window.URL || window.webkitURL;
-                    //    blobURL = url.createObjectURL(blob);
-                    //    image = dom.byId('attachment-image');
-                    //    image.onload = function() {
-                    //        self._sizeImage(self.domNode, image);
-                    //    }
-                    //    domAttr.set(image, 'src', blobURL);
-                        
-                   // });
-
                    //dataurl
-                    am.getAttachmentFile(attachmentid, 'arraybuffer', function(responseInfo) {
+                   am.getAttachmentFile(attachmentid, 'arraybuffer', function(responseInfo) {
                         var rData, url, a, dataUrl, image;
                         
                         rData = Utility.base64ArrayBuffer(responseInfo.response);
@@ -197,102 +174,42 @@ define('Mobile/SalesLogix/Views/Attachment/ViewAttachment', [
                             self._sizeImage(self.domNode, image);
                         }
                         domAttr.set(image, 'src', dataUrl);
-
+                        domClass.add(dl, 'display-none');
                     });
-                } else {
-
+               } else {
+                   viewNode = domConstruct.place(this.attachmentViewTemplate.apply(data, this), this.attachmentViewerNode, 'last');
+                   tpl = this.downloadingTemplate.apply(this);
+                   dl = domConstruct.place(tpl, this.attachmentViewerNode, 'first');
+                   domClass.add(this.domNode, 'list-loading');
+                   self = this;
+                   attachmentid = entry.$key;
+                   am.getAttachmentFile(attachmentid, 'arraybuffer', function(responseInfo) {
+                       var rData, url, a, dataUrl, iframe;
+                       rData = Utility.base64ArrayBuffer(responseInfo.response);
+                       dataUrl = 'data:' + responseInfo.contentType + ';base64,' + rData;
+                       domClass.add(dl, 'display-none');
+                       iframe = dom.byId('attachment-Iframe');
+                       iframe.onload = function() {
+                           domClass.add(dl, 'display-none');
+                       };
+                       domClass.add(dl, 'display-none');
+                       domAttr.set(iframe, 'src', dataUrl);
+                   });
                 }
             } else {
-
+                viewNode = domConstruct.place(this.attachmentViewTemplate.apply(data, this), this.attachmentViewerNode, 'last');
                 url = am.getAttachmenturlByEntity(entry);
-                window.open(url);
-            }
-
-        },
-        _loadAttachmentViewIOS: function(entry) {
-            var data, am, url, viewNode, tpl, dl, iframe, description;
-            am = new AttachmentManager();
-            url = am.getAttachmenturlByEntity(entry);
-            if (entry.dataType === "R") {
-                description = entry.description + ' (' + entry.fileName + ')';
-            } else {
-                description = entry.description + ' (' + entry.url + ')';
-            }
-            data = {
-                fileName: entry.fileName,
-                type: entry.type,
-                size: entry.size,
-                url: '',
-                description: description
-            };
-            this.imageZoomed = false;
-            viewNode = domConstruct.place(this.attachmentViewTemplate.apply(data, this), this.attachmentViewerNode, 'last');
-
-            domClass.add(this.domNode, 'list-loading');
-            tpl = this.downloadingTemplate.apply(this);
-            dl = domConstruct.place(tpl, this.attachmentViewerNode, 'first');
-
-            iframe = dom.byId('attachment-Iframe');
-            domClass.add(iframe, 'attachment-viewer-min');
-            iframe.onload = function() {
-                domClass.add(iframe, 'attachment-viewer-min');
+                domClass.add(this.domNode, 'list-loading');
+                tpl = this.downloadingTemplate.apply(this);
+                dl = domConstruct.place(tpl, this.attachmentViewerNode, 'first');
+                iframe = dom.byId('attachment-Iframe');
+                iframe.onload = function() {
+                    domClass.add(dl, 'display-none');
+                };
+                domAttr.set(iframe, 'src', url);
                 domClass.add(dl, 'display-none');
-            };
-            domAttr.set(iframe, 'src', url);
-
-        },
-        _loadAttachmentViewAndroid: function(entry) {
-            var data, am, isFile, url, viewNode, tpl, dl, iframe,description, attachmentid;
-            am = new AttachmentManager();
-            //url = am.getAttachmenturlByEntity(entry);
-            if (entry.dataType === "R") {
-                description = entry.description + ' (' + entry.fileName + ')';
-                isFile = true;
-            } else {
-                isFile = false;
-                description = entry.description + ' (' + entry.url + ')';
             }
-            data = {
-                fileName: entry.fileName,
-                type: entry.type,
-                size: entry.size,
-                url: '',
-                description: description
-            };
-            if (isFile) {
-                attachmentid = entry.$key;
-                //Blob url
-                am.getAttachmentFile(attachmentid, 'blob', function(responseInfo) {
-                    var blob, url, a, blobURL;
-                    blob = responseInfo.response;
-                    url = window.URL || window.webkitURL;
-                    blobURL = url.createObjectURL(blob);
-                    window.open(blobURL);
-                });
-              
 
-            } else {
-
-                url = am.getAttachmenturlByEntity(entry);
-                window.open(url);
-            }            
-
-        },
-        _loadAttachmentViewOther: function(entry) {
-            var data, am, url, viewNode
-
-            am = new AttachmentManager();
-            url = am.getAttachmenturlByEntity(entry);
-            data = {
-                fileName: entry.fileName,
-                type: entry.type,
-                size: entry.size,
-                url: '',
-                description: entry.description + ' (' + entry.fileName + ')'
-            };
-            viewNode = domConstruct.place(this.attachmentViewOtherTemplate.apply(data, this), this.attachmentViewerNode, 'last');
-            window.open(url);
-           // ReUI.back();
         },
         _zoomAttachment: function() {
 
@@ -320,45 +237,52 @@ define('Mobile/SalesLogix/Views/Attachment/ViewAttachment', [
             var wH, wW, iH, iW, contentBox, scale
 
            contentBox = domGeom.getContentBox(containerNode);
-            wH = contentBox.h - 100;
-            wW = contentBox.w -60;
+            wH = contentBox.h;
+            wW = contentBox.w;
             iH = image.height;
             iW = image.width;
+            scale = 1;
 
-            // if the image is smaller than the window do nothing
+            if (wH > 200) {
+
+                wH = wH - 50;
+            }
+            if (wW> 200) {
+
+                wW = wW - 50;
+            }
+            if (wH < 50) {
+
+                wH = 100;
+            }
+            if (wW < 50) {
+
+                wW = 100;
+            }
+            // if the image is larger than the window
             if(iW>wW && iH>wH){
-
+                // if the window height is lager than the width
                 if (wH < wW) {
                     scale = 1-((iH - wH) / iH);
-                    image.height = scale * iH; 
-                    image.width =  scale * iW;
-                } else {
+                } else { // if the window width is lager than the height
                     scale = 1-((iW - wW) / iW);
-                    image.height = scale * iH;
-                    image.width = scale * iW;
                 }
-            } else if (iW > wW) {
+            } else if (iW > wW) {// if the image  width is lager than the height
                 scale =1-((iW - wW) / iW);
-                image.height = scale * iH;
-                image.width = scale * iW;
             }
-            else if (iH > wH) {
+            else if (iH > wH) {// if the image  height is lager than the width 
                  scale = 1-((iH - wH) / iH);
-                image.height = scale * iH;
-                image.width = scale * iW;
-
             } else {
-               //Image is samller than view so we could zoom
+               //Image is samller than view
                 if (wH / iH > wW / iW) {
                     scale = 1+((wH - iH) / wH);
-                    image.height = scale * iH;
-                    image.width = scale * iW;
                 } else {
                     scale = 1+((wW - iW) / wW);
-                    image.height = scale * iH;
-                    image.width = scale * iW;
                 }
             }
+            image.height = scale * iH;
+            image.width = scale * iW;
+
         }
     });
 });
