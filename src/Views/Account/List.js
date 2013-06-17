95771ab967d5a9ad83dd3c7a94ca0739ce906207
Jason Best <skeeterbug@gmail.com>
2013-06-17 10:40:33 -0700

    Account list uses new utility function for joining fields.

--- src/Views/Account/List.js
+++ src/Views/Account/List.js
@@ -4,13 +4,15 @@ define('Mobile/SalesLogix/Views/Account/List', [
     'dojo/string',
     'Mobile/SalesLogix/Action',
     'Sage/Platform/Mobile/Format',
+    'Sage/Platform/Mobile/Utility',
     'Sage/Platform/Mobile/List'
 ], function(
     declare,
     array,
     string,
     action,
-    Format,
+    format,
+    utility,
     List
 ) {
 
@@ -20,13 +22,7 @@ define('Mobile/SalesLogix/Views/Account/List', [
             '<h3>{%: $.AccountName %}</h3>',
             '<h4>{%: $.Industry %}</h4>',
             '<h4>',
-            '{% if($.Type) { %}',
-                '{%: $.Type %}',
-            '{% } %}',
-            '{% if($.Type && $.SubType) { %} | {% } %}',
-            '{% if($.SubType) { %}',
-                '{%: $.SubType %}',
-            '{% } %}',
+                '{%: $$.joinFields(" | ", [$.Type, $.SubType]) %}',
             '</h4>',
             '<h4>{%: $.AccountManager && $.AccountManager.UserInfo ? $.AccountManager.UserInfo.UserName : "" %} | {%: $.Owner.OwnerDescription %}</h4>',
             '<h4>{%: $.WebAddress %}</h4>',
@@ -41,6 +37,10 @@ define('Mobile/SalesLogix/Views/Account/List', [
                 '</h4>',
             '{% } %}'
         ]),
+
+        joinFields: function(sep, fields) {
+            return utility.joinFields(sep, fields);
+        },
 
         //Localization
         titleText: 'Accounts',
