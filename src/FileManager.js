dfd90e221fee680cfe66dce1736e14bdcde91e07
Jason Best <skeeterbug@gmail.com>
2013-06-17 13:45:24 -0700

    [13092074] Fix android upload issue using BlobReader instead of Blob.

--- src/FileManager.js
+++ src/FileManager.js
@@ -87,6 +87,10 @@ define('Mobile/SalesLogix/FileManager', [
             }
         },
         _uploadFileHTML5_asBinary: function(file, url, progress, complete, error, scope, asPut) {
+            window.BlobBuilder = window.BlobBuilder || 
+                         window.WebKitBlobBuilder || 
+                         window.MozBlobBuilder || 
+                         window.MSBlobBuilder;
             if (!url) {
                 //assume Attachment SData url
                 url = 'slxdata.ashx/slx/system/-/attachments/file';// TODO: Remove this assumption from SDK
@@ -104,24 +108,33 @@ define('Mobile/SalesLogix/FileManager', [
 
             reader = new FileReader();
             reader.onload = lang.hitch(this, function(evt) {
-                var binary, boundary, dashdash, crlf, bb, unknownErrorText;
+                var binary, boundary, dashdash, crlf, bb, unknownErrorText, usingBlobBuilder, blobReader, blobData;
                 unknownErrorText = this.unknownErrorText;
-                bb = [];
+                blobReader = new FileReader();// read the blob as an ArrayBuffer to work around this android issue: https://code.google.com/p/android/issues/detail?id=39882
+                
+                try {
+                    new Blob();
+                    bb = [];
+                } catch(e) {
+                    bb = new window.BlobBuilder();
+                    usingBlobBuilder = true;
+                }
+
                 binary = evt.target.result;
                 boundary = "---------------------------" + (new Date()).getTime();
                 dashdash = '--';
                 crlf = '\r\n';
 
-                bb.push(dashdash + boundary + crlf);
-                bb.push('Content-Disposition: attachment; ');
-                bb.push('name="file_"; ');
-                bb.push('filename*="' + encodeURI(file.name) + '" ');
-                bb.push(crlf);
-                bb.push('Content-Type: ' + file.type);
-                bb.push(crlf + crlf);
-                bb.push(binary);
-                bb.push(crlf);
-                bb.push(dashdash + boundary + dashdash + crlf);
+                this._append(bb, dashdash + boundary + crlf);
+                this._append(bb, 'Content-Disposition: attachment; ');
+                this._append(bb, 'name="file_"; ');
+                this._append(bb, 'filename*="' + encodeURI(file.name) + '" ');
+                this._append(bb, crlf);
+                this._append(bb, 'Content-Type: ' + file.type);
+                this._append(bb, crlf + crlf);
+                this._append(bb, binary);
+                this._append(bb, crlf);
+                this._append(bb, dashdash + boundary + dashdash + crlf);
 
                 if (complete) {
                     request.onreadystatechange = function() {
@@ -143,11 +156,32 @@ define('Mobile/SalesLogix/FileManager', [
                         progress.call(scope || this, e);
                     });
                 }
+
                 request.setRequestHeader('Content-Type', 'multipart/attachment; boundary=' + boundary);
-                request.send(new Blob(bb));
+
+                if (usingBlobBuilder) {
+                    blobData = bb.getBlob(file.type);
+                } else {
+                    blobData = new Blob(bb);
+                }
+
+                // Read the blob back as an ArrayBuffer to work around this android issue:
+                // https://code.google.com/p/android/issues/detail?id=39882
+                blobReader.onload = function(e) {
+                    request.send(e.target.result);
+                }
+
+                blobReader.readAsArrayBuffer(blobData);
             });
 
             reader.readAsArrayBuffer(file);
+        },
+        _append: function(arrayOrBlobBuilder, data) {
+            if (arrayOrBlobBuilder && arrayOrBlobBuilder.constructor === Array) {
+                arrayOrBlobBuilder.push(data);
+            } else {
+                arrayOrBlobBuilder.append(data);
+            }
         },
         _onUnableToUploadError: function(msg, onError) {
             if (!msg) {
