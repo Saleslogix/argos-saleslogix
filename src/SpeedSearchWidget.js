31eddaa46a88e6bbcc7dc82990a864787c630dec
Jason Best <skeeterbug@gmail.com>
2013-07-01 15:58:35 -0700

    Updated the SpeedSearchWidget to use a placeholder attribute instead of a labelNode.

--- src/SpeedSearchWidget.js
+++ src/SpeedSearchWidget.js
@@ -20,11 +20,10 @@ define('Mobile/SalesLogix/SpeedSearchWidget', [
         widgetTemplate: new Simplate([
             '<div class="search-widget">',
             '<div class="table-layout">',
-                '<div><input type="text" name="query" class="query" autocorrect="off" autocapitalize="off" data-dojo-attach-point="queryNode" data-dojo-attach-event="onfocus:_onFocus,onblur:_onBlur,onkeypress:_onKeyPress" /></div>',
+                '<div><input type="text" placeholder="{%= $.searchText %}" name="query" class="query" autocorrect="off" autocapitalize="off" data-dojo-attach-point="queryNode" data-dojo-attach-event="onfocus:_onFocus,onblur:_onBlur,onkeypress:_onKeyPress" /></div>',
                 '<div class="hasButton"><button class="clear-button" tabindex="-1" data-dojo-attach-event="onclick: _onClearClick"></button></div>',
                 '<div class="hasButton"><button class="subHeaderButton searchButton" data-dojo-attach-event="click: search">{%= $.searchText %}</button></div>',
             '</div>',
-            '<label data-dojo-attach-point="labelNode">{%= $.searchText %}</label>',
             '</div>'
         ]),
         queryNode: null,
