b51ce26300d12d46aa7833265339e46f362ffd82
Jason Best <skeeterbug@gmail.com>
2013-07-09 16:00:59 -0700

    Revert "Fixed mixin to include the hashTagsNode attach point." Revert "Add icon for list header menu and action to show/hide the dom content."

--- src/Views/_MetricListMixin.js
+++ src/Views/_MetricListMixin.js
@@ -17,56 +17,30 @@ define('Mobile/SalesLogix/Views/_MetricListMixin', [
         metricWidgets: null,
         configurationView: 'metric_configure',
         entityName: '',
-        _insertedToolItem: false,
 
         postMixInProperties: function() {
             this.widgetTemplate =  new Simplate([
-            '<div id="{%= $.id %}" title="{%= $.titleText %}" class="overthrow list {%= $.cls %}" {% if ($.resourceKind) { %}data-resource-kind="{%= $.resourceKind %}"{% } %}>',
-            '{%! $.listHeaderTemplate %}',
-            '<a href="#" class="android-6059-fix">fix for android issue #6059</a>',                
-            '{%! $.emptySelectionTemplate %}',
-            '<ul class="list-content" data-dojo-attach-point="contentNode"></ul>',
-            '{%! $.moreTemplate %}',
-            '{%! $.listActionTemplate %}',
-            '</div>'
+                '<div id="{%= $.id %}" title="{%= $.titleText %}" class="overthrow list {%= $.cls %}" {% if ($.resourceKind) { %}data-resource-kind="{%= $.resourceKind %}"{% } %}>',
+                '<div data-dojo-attach-point="searchNode"></div>',
+                '<ul data-dojo-attach-point="metricNode" class="metric-list"></ul>',
+                '<a href="#" class="android-6059-fix">fix for android issue #6059</a>',
+                '{%! $.emptySelectionTemplate %}',
+                '<ul class="list-content" data-dojo-attach-point="contentNode"></ul>',
+                '{%! $.moreTemplate %}',
+                '{%! $.listActionTemplate %}',
+                '</div>'
             ]);
         },
-        /**
-         * @property {Simplate}
-         * The template used to render the list views header menu.
-         *
-         */
-        listHeaderTemplate: new Simplate([
-            '<div class="list-header list-header-hidden" data-dojo-attach-point="listHeader">',
-                '<div data-dojo-attach-point="searchNode"></div>',
-                '<div class="list-hash-tags" data-dojo-attach-point="hashTagsNode"></div>',
-                '<ul data-dojo-attach-point="metricNode" class="metric-list"></ul>',
-            '</div>'
-        ]),
         createMetricWidgetsLayout: function() {
             return App.preferences && App.preferences.metrics && App.preferences.metrics[this.resourceKind];
         },
         createToolLayout: function() {
-            this.inherited(arguments);
-            var tbarItem = {
-                id: 'configure',
-                action: 'navigateToConfigurationView'
-            };
-
-            if (this.tools && this.tools.tbar) {
-                if (!this._insertedToolItem) {
-                    this.tools.tbar.unshift(tbarItem);
-                    this._insertedToolItem = true;
-                }
-            } else {
-                this.tools = {
-                    tbar: [
-                        tbarItem
-                    ]
-                };
-            }
-
-            return this.tools;
+            return this.tools || (this.tools = {
+                tbar: [{
+                    id: 'configure',
+                    action: 'navigateToConfigurationView'
+                }]
+            });
         },
         navigateToConfigurationView: function() {
             var view = App.getView(this.configurationView);
@@ -78,7 +52,6 @@ define('Mobile/SalesLogix/Views/_MetricListMixin', [
         },
         postCreate: function() {
             this.inherited(arguments);
-
         },
         destroyWidgets: function() {
             array.forEach(this.metricWidgets, function(widget) {
