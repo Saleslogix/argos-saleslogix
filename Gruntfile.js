c370e801e9b550f81d9549f1ecc468f537c2242d
Jason Best <skeeterbug@gmail.com>
2013-07-19 09:46:36 -0700

    Added grunt tasks for csslint and cssmin. Updated build script to use minified css. Updated index pages to use minified file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,32 @@ module.exports = function(grunt) {
                 host: 'http://127.0.0.1:8000/products/argos-saleslogix/',
                 template: 'GruntRunner.tmpl'
             }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'min/css/app.min.css': ['content/css/toggle.css', 'content/css/app.css']
+                }
+            }
+        },
+        csslint: {
+            options: {
+                csslintrc: '.csslintrc',
+                formatters: [
+                    { id: 'junit-xml', dest: 'report/junit.xml' }
+                ]
+            },
+            lax: {
+                src: ['content/**/*.css']
+            }
         }
     });
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
 
     grunt.registerTask('test', ['connect', 'jasmine']);
     grunt.registerTask('default', ['test']);
