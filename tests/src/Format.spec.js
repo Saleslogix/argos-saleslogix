16b05d5f6e512b3121d00d3577b354c866eebf34
Jason Best <jason.best@swiftpage.com>
2013-08-05 10:33:01 -0700

    Added billion test. Fixed broken million since 10B is no longer 10,000M.

--- tests/src/Format.spec.js
+++ tests/src/Format.spec.js
@@ -28,9 +28,12 @@ define('spec/Format.spec', ['Mobile/SalesLogix/Format'],function(Format) {
         });
 
         describe('bigNumber', function() {
+            it('should contain an B', function() {
+                expect(Format.bigNumber(9999999999)).toEqual('10.0B');
+            });
+
             it('should contain an M', function() {
                 expect(Format.bigNumber(1000000)).toEqual('1.0M');
-                expect(Format.bigNumber(9999999999)).toEqual('10,000.0M');
             });
 
             it('should contain a K', function() {
