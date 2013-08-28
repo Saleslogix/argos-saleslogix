31b03d2d19334d9ada2cd65b1f77a3c87c9ac01b
Jason Best <jason.best@swiftpage.com>
2013-08-28 09:46:52 -0700

    Fix back button not working correctly under certain scenarios with kpi charts. #MBL-10006 Fixed

--- src/Views/Charts/GenericPie.js
+++ src/Views/Charts/GenericPie.js
@@ -39,6 +39,10 @@ define('Mobile/SalesLogix/Views/Charts/GenericPie', [
             chartContent: {node: 'contentNode', type: 'innerHTML'}
         },
 
+        getTag: function() {
+            return this.options && this.options.returnTo;
+        },
+
         widgetTemplate: new Simplate([
             '<div id="{%= $.id %}" title="{%= $.titleText %}" class="list {%= $.cls %}">',
                 '<div class="chart-content" data-dojo-attach-point="contentNode"></div>',
