46c01634f42d97965416b3eec53d3e48352041eb
Jason Best <jason.best@swiftpage.com>
2014-01-13 14:53:52 -0700

    Create a main for jasmine tests.

---
+++ tests/src/all.js
@@ -0,0 +1,16 @@
+define([
+    'spec/Aggregate.spec',
+    'spec/Views/MetricWidget.spec',
+    'spec/Application.spec', 
+    'spec/Format.spec',
+    'spec/Utility.spec',
+    'spec/Validator.spec',
+    'spec/Fields/AddressField.spec',
+    'spec/Fields/MultiCurrencyField.spec',
+    'spec/Fields/NameField.spec',
+    'spec/Fields/PicklistField.spec',
+    'spec/Fields/RecurrencesField.spec',
+    'spec/Views/ExchangeRateLookup.spec',
+    'localization/en',
+    'localization/saleslogix/en'
+], function() {});
