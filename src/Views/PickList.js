aa10fe75d3d65d7a10a8feaf79aaa9a90a6ee77d
Jason Best <jason.best@swiftpage.com>
2014-04-30 15:15:31 -0700

    Fix picklist select issue.

--- src/Views/PickList.js
+++ src/Views/PickList.js
@@ -33,12 +33,14 @@ define('Mobile/SalesLogix/Views/PickList', [
 
         show: function(options) {
             this.set('title', options && options.title || this.title);
-            if (options.keyProperty) {
-                this.keyProperty = options.keyProperty;
-            }
+            if (!options.singleSelect) {
+                if (options.keyProperty) {
+                    this.keyProperty = options.keyProperty;
+                }
 
-            if (options.textProperty) {
-                this.labelProperty = options.textProperty;
+                if (options.textProperty) {
+                    this.labelProperty = options.textProperty;
+                }
             }
 
             this.inherited(arguments);
