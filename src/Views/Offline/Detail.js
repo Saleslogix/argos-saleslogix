16172329d614ff088fda48748c7a2683f0d33749
Jason Best <Jason.Best@swiftpage.com>
2014-10-16 11:07:54 -0700

    Stubbing out offline list/detail views. #MBL-10664

---
+++ src/Views/Offline/Detail.js
@@ -0,0 +1,20 @@
+/**
+ * @class Mobile.SalesLogix.Views.Offline.Detail
+ *
+ *
+ * @extends Sage.Platform.Mobile.Detail
+ * @requires Sage.Platform.Mobile.Detail
+ *
+ */
+define('Mobile/SalesLogix/Views/Offline/Detail', [
+    'dojo/_base/declare',
+    'Sage/Platform/Mobile/Detail'
+], function(
+    declare,
+    Detail
+    ) {
+
+    return declare('Mobile.SalesLogix.Views.Offline.Detail', [Detail], {
+    });
+});
+
---
+++ src/Views/Offline/List.js
@@ -0,0 +1,21 @@
+/**
+ * @class Mobile.SalesLogix.Views.Offline.List
+ *
+ * @extends Sage.Platform.Mobile.List
+ * @requires Sage.Platform.Mobile.List
+ *
+ *
+ */
+define('Mobile/SalesLogix/Views/Offline/List', [
+    'dojo/_base/declare',
+    'Sage/Platform/Mobile/List'
+], function(
+    declare,
+    List
+    ) {
+
+    return declare('Mobile.SalesLogix.Views.Offline.List', [List], {
+
+    });
+});
+
