52e1a78bfd71d4e2c408631e8be12664107d22a0
Jason Best <Jason.Best@swiftpage.com>
2014-10-16 14:49:19 -0700

    Offline detail now loads the offline doc. Nothing is currently rendered. A dynamic layout must be generated based on the offline doc. #MBL-10664

--- src/Views/Offline/Detail.js
+++ src/Views/Offline/Detail.js
@@ -8,15 +8,30 @@
  */
 define('Mobile/SalesLogix/Views/Offline/Detail', [
     'dojo/_base/declare',
-    'Sage/Platform/Mobile/_DetailBase'
+    'Sage/Platform/Mobile/_DetailBase',
+    'Sage/Platform/Mobile/Store/PouchDB'
 ], function(
     declare,
-    _DetailBase
+    _DetailBase,
+    Store
     ) {
 
     return declare('Mobile.SalesLogix.Views.Offline.Detail', [_DetailBase], {
         id: 'offline_detail',
-        titleText: 'Offline Detail'
+        titleText: 'Offline Detail',
+
+        idProperty: 'id',
+
+        OFFLINE_DB_NAME: 'crm-offline',
+        createStore: function() {
+            return new Store({databaseName: this.OFFLINE_DB_NAME});
+        },
+        _applyStateToGetOptions: function(getOptions) {
+        },
+        _buildGetExpression: function() {
+            var options = this.options;
+            return options && (options.id || options.key);
+        }
     });
 });
 
