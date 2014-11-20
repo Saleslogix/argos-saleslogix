eb38bb0c473b45b2fc0b96350e94d8a7ca2e1c6e
Jason Best <jason.best@infor.com>
2014-11-20 11:24:00 -0700

    Updating styles for the speedsearch widget. #MBL-10660

--- src/SpeedSearchWidget.js
+++ src/SpeedSearchWidget.js
@@ -38,8 +38,6 @@ define('Mobile/SalesLogix/SpeedSearchWidget', [
             '<div class="search-widget">',
             '<div class="table-layout">',
                 '<div><input type="text" placeholder="{%= $.searchText %}" name="query" class="query" autocorrect="off" autocapitalize="off" data-dojo-attach-point="queryNode" data-dojo-attach-event="onfocus:_onFocus,onblur:_onBlur,onkeypress:_onKeyPress" /></div>',
-                '<div class="hasButton"><button class="clear-button" tabindex="-1" data-dojo-attach-event="onclick: _onClearClick"></button></div>',
-                '<div class="hasButton"><button class="subHeaderButton searchButton" data-dojo-attach-event="click: search">{%= $.searchText %}</button></div>',
             '</div>',
             '</div>'
         ]),
@@ -96,6 +94,11 @@ define('Mobile/SalesLogix/SpeedSearchWidget', [
         },
         _onFocus: function() {
             domClass.add(this.domNode, 'search-active');
+
+            // Work around a chrome issue were mouseup after a focus will de-select the text
+            setTimeout(function() {
+                this.queryNode.select();
+            }.bind(this), 50);
         },
         _onKeyPress: function(evt) {
             if (evt.keyCode == 13 || evt.keyCode == 10) {
