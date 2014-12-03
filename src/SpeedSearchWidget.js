e4f761d6b25d47bceb0f00db0f0839dec80addbd
Jason Best <jason.best@infor.com>
2014-12-03 13:13:30 -0700

    switching from select to setSelectionRange to work around IOS issues. #MBL-10764

--- src/SpeedSearchWidget.js
+++ src/SpeedSearchWidget.js
@@ -95,9 +95,9 @@ define('Mobile/SalesLogix/SpeedSearchWidget', [
         _onFocus: function() {
             domClass.add(this.domNode, 'search-active');
 
-            // Work around a chrome issue were mouseup after a focus will de-select the text
+            // Work around a chrome issue where mouseup after a focus will de-select the text
             setTimeout(function() {
-                this.queryNode.select();
+                this.queryNode.setSelectionRange(0, 9999);
             }.bind(this), 50);
         },
         _onKeyPress: function(evt) {
