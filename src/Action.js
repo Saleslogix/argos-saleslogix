8f36d6c50c244c44a8a88cb001636e97f85e5b74
Cameron Henrie <cameron.henrie@infor.com>
2014-12-09 14:52:08 -0700

    Fixed #MBL-10785 Ticket Group list phone number

--- src/Action.js
+++ src/Action.js
@@ -47,6 +47,7 @@ define('Mobile/SalesLogix/Action', [
             this.navigateToHistoryInsert(entry, complete);
         },
         callPhone: function(action, selection, phoneProperty) {
+            var value;
             this.setSource({
                 entry: selection.data,
                 descriptor: selection.data['$descriptor'],
@@ -57,19 +58,20 @@ define('Mobile/SalesLogix/Action', [
                 'Type': 'atPhoneCall',
                 'Description': string.substitute(Mobile.SalesLogix.Action.calledText, [selection.data['$descriptor']])
             });
-
+            value = utility.getValue(selection.data, phoneProperty, '');
             Mobile.SalesLogix.Action.recordToHistory(function() {
-                App.initiateCall(selection.data[phoneProperty]);
+                App.initiateCall(value);
             }.bindDelegate(this), selection.data);
         },
         sendEmail: function(action, selection, emailProperty) {
+            var value;
             lang.mixin(selection.data, {
                 'Type': 'atEmail',
                 'Description': string.substitute(Mobile.SalesLogix.Action.emailedText, [selection.data['$descriptor']])
             });
-
+            value = utility.getValue(selection.data, emailProperty, '');
             Mobile.SalesLogix.Action.recordToHistory(function() {
-                App.initiateEmail(selection.data[emailProperty]);
+                App.initiateEmail(value);
             }.bindDelegate(this), selection.data);
         },
 
