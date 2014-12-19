112e49b15fd37bc5cc3ec97cad8975887a6f8dc9
Jason Best <jason.best@infor.com>
2014-12-19 15:49:53 -0700

    Bar chart ported to chartjs.

--- src/Views/Charts/GenericPie.js
+++ src/Views/Charts/GenericPie.js
@@ -26,136 +26,11 @@ define('Mobile/SalesLogix/Views/Charts/GenericPie', [
     View,
     _ChartMixin
 ) {
-    window.Chart.defaults.global = {
-        // Boolean - Whether to animate the chart
-        animation: true,
-
-        // Number - Number of animation steps
-        animationSteps: 60,
-
-        // String - Animation easing effect
-        animationEasing: "easeOutQuart",
-
-        // Boolean - If we should show the scale at all
-        showScale: true,
-
-        // Boolean - If we want to override with a hard coded scale
-        scaleOverride: false,
-
-        // ** Required if scaleOverride is true **
-        // Number - The number of steps in a hard coded scale
-        scaleSteps: null,
-        // Number - The value jump in the hard coded scale
-        scaleStepWidth: null,
-        // Number - The scale starting value
-        scaleStartValue: null,
-
-        // String - Colour of the scale line
-        scaleLineColor: "rgba(0,0,0,.1)",
-
-        // Number - Pixel width of the scale line
-        scaleLineWidth: 1,
-
-        // Boolean - Whether to show labels on the scale
-        scaleShowLabels: true,
-
-        // Interpolated JS string - can access value
-        scaleLabel: "<%=value%>",
-
-        // Boolean - Whether the scale should stick to integers, not floats even if drawing space is there
-        scaleIntegersOnly: true,
-
-        // Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero: false,
-
-        // String - Scale label font declaration for the scale label
-        scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-
-        // Number - Scale label font size in pixels
-        scaleFontSize: 12,
-
-        // String - Scale label font weight style
-        scaleFontStyle: "normal",
-
-        // String - Scale label font colour
-        scaleFontColor: "#666",
-
-        // Boolean - whether or not the chart should be responsive and resize when the browser does.
-        responsive: true,
-
-        // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-        maintainAspectRatio: true,
-
-        // Boolean - Determines whether to draw tooltips on the canvas or not
-        showTooltips: true,
-
-        // Array - Array of string names to attach tooltip events
-        tooltipEvents: ["mousemove", "touchstart", "touchmove"],
-
-        // String - Tooltip background colour
-        tooltipFillColor: "rgba(0,0,0,0.8)",
-
-        // String - Tooltip label font declaration for the scale label
-        tooltipFontFamily: "'Helvetica', 'Arial', Sans-serif",
-
-        // Number - Tooltip label font size in pixels
-        tooltipFontSize: 14,
-
-        // String - Tooltip font weight style
-        tooltipFontStyle: "normal",
-
-        // String - Tooltip label font colour
-        tooltipFontColor: "#fff",
-
-        // String - Tooltip title font declaration for the scale label
-        tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-
-        // Number - Tooltip title font size in pixels
-        tooltipTitleFontSize: 14,
-
-        // String - Tooltip title font weight style
-        tooltipTitleFontStyle: "bold",
-
-        // String - Tooltip title font colour
-        tooltipTitleFontColor: "#fff",
-
-        // Number - pixel width of padding around tooltip text
-        tooltipYPadding: 6,
-
-        // Number - pixel width of padding around tooltip text
-        tooltipXPadding: 6,
-
-        // Number - Size of the caret on the tooltip
-        tooltipCaretSize: 8,
-
-        // Number - Pixel radius of the tooltip border
-        tooltipCornerRadius: 6,
-
-        // Number - Pixel offset from point x to tooltip edge
-        tooltipXOffset: 10,
-
-        // String - Template string for single tooltips
-        tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
-
-        // String - Template string for single tooltips
-        multiTooltipTemplate: "<%= value %>",
-
-        // Function - Will fire on animation progression.
-        onAnimationProgress: function(){},
-
-        // Function - Will fire on animation completion.
-        onAnimationComplete: function(){}
-    };
 
     return declare('Mobile.SalesLogix.Views.Charts.GenericPie', [View, _ChartMixin], {
         id: 'chart_generic_pie',
         titleText: '',
-        otherText: 'Other',
-        expose: false,
         chart: null,
-        MAX_ITEMS: 5,
-        pieColor: '#0896e9',
-        stroke: '#ffffff',
         seriesColors: [
             '#1c9a18',
             '#6ec90d',
@@ -182,7 +57,7 @@ define('Mobile/SalesLogix/Views/Charts/GenericPie', [
         createChart: function(rawData) {
             this.inherited(arguments);
 
-            var ctx, box, searchExpressionHeight;
+            var ctx, box, searchExpressionHeight, data;
 
             this.showSearchExpression();
             searchExpressionHeight = this.getSearchExpressionHeight();
@@ -190,7 +65,7 @@ define('Mobile/SalesLogix/Views/Charts/GenericPie', [
             box = domGeo.getMarginBox(this.domNode);
             box.h = box.h - searchExpressionHeight;
 
-            var data = array.map(rawData, function(item, idx) {
+            data = array.map(rawData, function(item, idx) {
                 return {
                     value: Math.round(item.value),
                     color: this._getItemColor(idx),
@@ -209,9 +84,11 @@ define('Mobile/SalesLogix/Views/Charts/GenericPie', [
 
             ctx = this.contentNode.getContext('2d');
 
-            this.chart = new window.Chart(ctx).Pie(data, {
-                segmentShowStroke: false,
-                animateScale: true
+            this.chart = new window.Chart(ctx).Doughnut(data, {
+                segmentShowStroke: true,
+                segmentStrokeColor: '#EBEBEB',
+                segmentStrokeWidth: 5,
+                animateScale: false
             });
         },
         _getItemColor: function(index) {
