3d3a5256d7145c87622102e718f851475c9b943f
Jason Best <jason.best@infor.com>
2015-01-13 17:11:19 -0700

    Adding a batch script to run the bundler.

---
+++ build/bundle.bat
@@ -0,0 +1,24 @@
+@echo off
+SET SDK=%CD%\..\..\argos-sdk
+
+rmdir /S /Q deploy
+rmdir /S /Q %SDK%\deploy
+
+mkdir deploy
+mkdir %SDK%\deploy
+mkdir %SDK%\deploy\temp
+mkdir deploy\bundle
+mkdir deploy\bundle\model
+xcopy bundle\model\*.* deploy\bundle\model /E /Y
+
+mkdir deploy\bundle\model\Portal\SlxMobile\SourceFiles
+mkdir deploy\bundle\model\Portal\SlxMobile\SourceFiles\argos-sdk
+mkdir deploy\bundle\model\Portal\SlxMobile\SourceFiles\products
+mkdir deploy\bundle\model\Portal\SlxMobile\SourceFiles\products\argos-saleslogix
+
+xcopy %SDK%\*.* deploy\bundle\model\Portal\SlxMobile\SourceFiles\argos-sdk /E /Y /exclude:build\bundleExcludes.txt
+xcopy *.* %SDK%\deploy\temp /E /Y /exclude:build\bundleExcludes.txt
+xcopy %SDK%\deploy\temp\*.* deploy\bundle\model\Portal\SlxMobile\SourceFiles\products\argos-saleslogix /E /Y
+rmdir %SDK%\deploy\temp /S /Q
+
+%SDK%\tools\bundler\Bundler.exe /ProjectPath:"%CD%\deploy\bundle\model" /BundleFileName:"%CD%\deploy\bundle.zip" /BundleMethod:All /ConfigFileName:"%CD%\build\bundle.config"
