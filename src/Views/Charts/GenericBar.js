c9d68ea7ebdfa1a5dd899b60e2f54be9a08c10b0
Jason Best <jason.best@infor.com>
2015-02-19 16:02:37 -0700

    Fix bar chart with negative values issue. #MBL-10842

--- src/Views/Charts/GenericBar.js
+++ src/Views/Charts/GenericBar.js
@@ -36,6 +36,7 @@ define('crm/Views/Charts/GenericBar', [
         barColor: '#0896e9',
 
         chartOptions: {
+            scaleBeginAtZero: false,
             barShowStroke: false,
             legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
         },
