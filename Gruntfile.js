c9f1ac3bdaf4847bfd9abff35c8fd05dd9708151
Jason Best <jason.best@infor.com>
2015-03-20 14:07:38 -0700

    Remove check from test task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
 
     // Register alias tasks
     grunt.registerTask('check', ['jshint', 'jscs']);
-    grunt.registerTask('test', ['check', 'connect', 'jasmine:coverage']);
+    grunt.registerTask('test', ['connect', 'jasmine:coverage']);
     grunt.registerTask('server', ['connect:server:keepalive']);
     grunt.registerTask('bundle', ['shell:bundle:<%= pkg.version %>']);
     grunt.registerTask('default', ['test']);
