b7dac52602e663d2c8caa14eaef3694b7052206f
Cameron Henrie <cameron.henrie@Infor.com>
2015-04-22 15:37:50 -0700

    Adding zh Chinese local

---
+++ localization/zh-CN.js
@@ -0,0 +1,56 @@
+﻿define('localization/en', ['dojo/_base/lang'], function(lang) {
+
+    return lang.setObject('Mobile.CultureInfo', {
+      "name": "en",
+      "englishName": "English (United States)",
+      "nativeName": "English (United States)",
+      "numberFormat": {
+        "currencyDecimalDigits": 2,
+        "currencyDecimalSeparator": ".",
+        "isReadOnly": false,
+        "currencyGroupSizes": [
+          3
+        ],
+        "numberGroupSizes": [
+          3
+        ],
+        "percentGroupSizes": [
+          3
+        ],
+        "currencyGroupSeparator": ",",
+        "currencySymbol": "$",
+        "naNSymbol": "NaN",
+        "currencyNegativePattern": 0,
+        "numberNegativePattern": 1,
+        "percentPositivePattern": 0,
+        "percentNegativePattern": 0,
+        "negativeInfinitySymbol": "-Infinity",
+        "negativeSign": "-",
+        "numberDecimalDigits": 2,
+        "numberDecimalSeparator": ".",
+        "numberGroupSeparator": ",",
+        "currencyPositivePattern": 0,
+        "positiveInfinitySymbol": "Infinity",
+        "positiveSign": "+",
+        "percentDecimalDigits": 2,
+        "percentDecimalSeparator": ".",
+        "percentGroupSeparator": ",",
+        "percentSymbol": "%",
+        "perMilleSymbol": "‰",
+        "nativeDigits": [
+          "0",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9"
+        ],
+        "digitSubstitution": 1
+      }
+    });
+
+});
