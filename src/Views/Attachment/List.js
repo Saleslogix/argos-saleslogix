ec0557d8a1624c5cafafefe4877e3cc24c1f013a
Cameron Henrie <cameron.henrie@infor.com>
2015-06-26 14:30:57 -0700

    Fixing Attachment Hashtags

--- src/Views/Attachment/List.js
+++ src/Views/Attachment/List.js
@@ -110,8 +110,8 @@ define('crm/Views/Attachment/List', [
         queryInclude: ['$descriptors'],
 
         hashTagQueries: {
-            'url': 'url ne null',
-            'binary': 'url eq null'
+            'url': "(fileName like '%.URL')",
+            'binary': "(fileName not like '%.URL')"
         },
         hashTagQueriesText: {
             'url': 'url',
