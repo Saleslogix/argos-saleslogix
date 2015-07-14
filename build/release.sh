133eabeeffd12858d3f3cb5c3b7b7d3bffe2a776
Jason Best <jason.best@infor.com>
2015-07-13 17:11:46 -0700

    Release script working on mac. This includes the shell script and "grunt release".

--- build/release.sh
+++ build/release.sh
@@ -14,7 +14,7 @@ mkdir -p deploy/content/css
 # mono ../../argos-sdk/tools/JsBit/JsBit.exe -p "build/release.jsb2" -d "."
 
 # Java Build Tool
-$JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -jar "../../argos-sdk/tools/JSBuilder/JSBuilder2.jar" -v -p "build/release.jsb2" -d "."
+java -Dfile.encoding=UTF-8 -jar "../../argos-sdk/tools/JSBuilder/JSBuilder2.jar" -v -p "build/release.jsb2" -d "."
 
 cp index.html ./deploy
 cp index.aspx ./deploy
--- grunt-tasks/grunt-shell.js
+++ grunt-tasks/grunt-shell.js
@@ -1,7 +1,13 @@
+var os = require('os');
+
 module.exports = function(grunt) {
     grunt.config('shell', {
         bundle: {
             command: function(version) {
+                if (os.platform() !== 'win32') {
+                    throw new Error('The bundle command is not available on non-windows platforms.');
+                }
+                
                 var cmd = 'build\\bundle';
                 if (version) {
                     return [cmd, ' ', version].join('');
@@ -19,7 +25,7 @@ module.exports = function(grunt) {
             }
         },
         release: {
-            command: 'build\\release.cmd',
+            command: os.platform() === 'win32' ? 'build\\release.cmd' : 'build/release.sh',
             options: {
                 execOptions: {
                     cwd: ''
@@ -38,4 +44,3 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-shell');
 };
-
