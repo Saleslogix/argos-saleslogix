cc670e2c3fbd98db249692f38e7d23c29249deb3
Chenrie <cameron.henrie@infor.com>
2015-09-24 22:25:28 -0700

    Adding OfflineModelBase

--- src/Models/Offline/Activity.js
+++ src/Models/Offline/Activity.js
@@ -1,10 +1,10 @@
 import declare from 'dojo/_base/declare';
-import _ModelBase from 'argos/Models/_ModelBase';
+import _OfflineModelBase from 'argos/Models/_OfflineModelBase';
 import Manager from 'argos/Models/Manager';
 import MODEL_TYPE from 'argos/Models/Types';
 import MODEL_NAMES from '../Names';
 
-const __class = declare('crm.Models.Offline.Activity', [_ModelBase], {
+const __class = declare('crm.Models.Offline.Activity', [_OfflineModelBase], {
 });
 
 Manager.register(MODEL_NAMES.ACTIVITY, MODEL_TYPE.OFFLINE, __class);
