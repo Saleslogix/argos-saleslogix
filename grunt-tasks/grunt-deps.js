cf90a590ddd0001ea3f850b2430dc47f13dabf5f
Jason Best <jason.best@infor.com>
2015-10-08 17:28:59 -0700

    Starting work to build up the graph.

--- grunt-tasks/grunt-deps.js
+++ grunt-tasks/grunt-deps.js
@@ -21,19 +21,33 @@ module.exports = function gruntDeps(grunt) {
   grunt.registerTask('deps', function() {
     var config = grunt.config.get('deps');
     var files = grunt.file.expand(config.files);
+    var graph = new Graph();
+    var nodes = {};
+
+    // TODO: We will populate all the nodes here,
+    // We can optimize this and add nodes/edges as we go in the second loop,
+    // and remove this one.
+    files.forEach(function(f) {
+      nodes[f] = {
+        name: f
+      };
+      graph.add(nodes[f]);
+    });
+
     files.forEach(function(file) {
       var contents = grunt.file.read(file, { encoding: 'utf8' });
       try {
-        var tree = esprima.parse(contents, { tolerant: true, comment: true });
+        var tree = esprima.parse(contents, { tolerant: true });
         var body = tree.body;
-        body = body.filter(function(node) {
+        var modules = body.filter(function(node) {
           return node.type === 'ImportDeclaration';
         }).map(function(node) {
           return {
             module: node.source.value
           };
         });
-        grunt.log.writeln(JSON.stringify(body, null, 2));
+        // TODO: Resolve the modules to a file path
+        grunt.log.writeln(JSON.stringify(modules, null, 2));
       } catch (error) {
         grunt.log.writeln(file);
         grunt.log.writeln(error);
