31660db2b8df81883c563fb9535bf2909f781412
Chenrie <cameron.henrie@infor.com>
2015-11-11 16:33:53 -0700

    Adding removal of completed activities form offline storage areas

--- src/Models/Activity/SData.js
+++ src/Models/Activity/SData.js
@@ -149,7 +149,20 @@ const __class = declare('crm.Models.Activity.SData', [Base, _SDataModelBase], {
       success: function success() { // Can we get back the history to add to the Offline?
         const oModel = App.ModelManager.getModel(this.modelName, MODEL_TYPES.OFFLINE);
         if (oModel) {
-          oModel.deleteEntry(entry.$key);
+          try {
+            oModel.deleteEntry(entry.$key);
+            // Todo add observable or event to remove delete items out of brifecase and recently viewed areas.
+            const rvModel = App.ModelManager.getModel('RecentlyViewed', MODEL_TYPES.OFFLINE);
+            const bcModel = App.ModelManager.getModel('Briefcase', MODEL_TYPES.OFFLINE);
+            if (rvModel) {
+              rvModel.deleteEntryByEntityContext(entry.$key, this.entityName);
+            }
+            if (bcModel) {
+              bcModel.deleteEntryByEntityContext(entry.$key, this.entityName);
+            }
+          } catch(err) {
+            console.log(err); // eslint-disable-line
+          }
         }
 
         def.resolve();
