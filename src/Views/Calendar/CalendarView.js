791c52c04856a50541e70a76b15dbb056ec03889
Tyler Wallace <tylerw710@gmail.com>
2015-12-09 10:27:53 -0700

    INFORCRM-7202: Overriding createToolLayout to include a refresh

--- src/Views/Calendar/CalendarView.js
+++ src/Views/Calendar/CalendarView.js
@@ -50,7 +50,7 @@ const __class = declare('crm.Views.Calendar.CalendarView', [List], {
   dayText: resource.dayText,
   weekText: resource.weekText,
 
-  enablePullToRefresh: false,
+  enablePullToRefresh: true,
   string: string,
   Utility: Utility,
   trimTo: 16,
@@ -365,6 +365,20 @@ const __class = declare('crm.Views.Calendar.CalendarView', [List], {
     store.include = this.eventInclude;
     this._eventStore = store;
     return store;
+  },
+  createToolLayout: function createToolLayout() {
+    return this.tools || (this.tools = {
+      'tbar': [{
+        id: 'new',
+        cls: 'fa fa-plus fa-fw fa-lg',
+        action: 'navigateToInsertView',
+        security: this.app.getViewSecurity(this.insertView, 'insert'),
+      }, {
+        id: 'refresh',
+        cls: 'fa fa-refresh fa-fw fa-lg',
+        action: 'refresh',
+      }],
+    });
   },
   formatQueryActivity: function formatQueryActivity(value) {
     return string.substitute('UserActivities.UserId eq "${user}" and Type ne "atLiterature" and StartDate between @${start}@ and @${end}@', {
