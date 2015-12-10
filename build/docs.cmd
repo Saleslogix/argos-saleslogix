67f90cf52b0b0052cd905c1999fed1a47bc8272d
Jason Best <jason.best@infor.com>
2015-12-10 15:37:31 -0700

    Updating doc categories and doc script.

--- build/docs.cmd
+++ build/docs.cmd
@@ -1,5 +1,11 @@
 @echo off
+DEL /Q /F docs-output
 call grunt clean:js babel
+
+pushd ..\..\argos-sdk
+call grunt clean:js babel
+popd
+
 ..\..\argos-sdk\tools\jsduck\jsduck.exe --config=src-docs\docs-config.json
 
 EXIT /B 0
