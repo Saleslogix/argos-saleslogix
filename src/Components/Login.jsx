ece8dd9658ddbb25f0dfe62659eeef545908d26a
Jason Best <skeeterbug@gmail.com>
2017-01-11 17:00:04 -0700

    Login work.

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,8 +2,19 @@ export default class LoginView extends React.Component {
   render() {
     return (
       <div>
-        <span>Username: </span>
-        <span>Password: </span>
+        <p className="logo">
+          <img src="content/images/logo-64.png"></img>
+          <span>Infor CRM</span>
+        </p>
+        <div className="panel-content">
+          <h2>Details</h2>
+          <fieldset>
+            <a name="username"></a>
+            <div className="row row-edit" data-field-type="text">
+              
+            </div>
+          </fieldset>
+        </div>
       </div>
     );
   }
