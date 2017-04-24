2e90f623acd20321598e23faef2b7dfed2057b40
pbal <pukhraj.bal@infor.com>
2017-04-24 13:17:12 -0700

    INFORCRM-3048: compare to lower case for moment locale

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -107,9 +107,9 @@ export default function bootstrap({
 
           // moment will return the set culture if successful, otherwise it returns the currently set culture.
           // Check to see if the culture set failed, and attept to use the specific culture instead
-          if (results !== parentRegionLocale) {
+          if (results !== parentRegionLocale.toLocaleLowerCase()) {
             results = moment.locale(currentRegionLocale);
-            if (results !== currentRegionLocale) {
+            if (results !== currentRegionLocale.toLocaleLowerCase()) {
               console.error(`Failed to set the culture for moment.js, culture set to ${results}`); // eslint-disable-line
             }
           }
