cbcf3d7eb420b512cfff941cb415695fe6977af0
Jason Best <jason.best@infor.com>
2017-05-04 11:09:41 -0400

    INFORCRM-16214: Moving babel-polyfill, less, moment, redux, and rxjs to libraries from node_modules.

--- build/bundle.bat
+++ build/bundle.bat
@@ -11,7 +11,6 @@ rmdir /S /Q %SDK%\deploy
 
 mkdir deploy
 mkdir %SDK%\deploy
-mkdir %SDK%\deps
 mkdir %SDK%\deploy\temp
 mkdir deploy\bundle
 mkdir deploy\bundle\model
@@ -22,11 +21,6 @@ mkdir deploy\bundle\model\Portal\SlxMobile\SourceFiles\argos-sdk
 mkdir deploy\bundle\model\Portal\SlxMobile\SourceFiles\products
 mkdir deploy\bundle\model\Portal\SlxMobile\SourceFiles\products\argos-saleslogix
 
-xcopy %SDK%\node_modules\babel-polyfill\dist\polyfill.min.js %SDK%\deps\babel-polyfill\ /E /Y
-xcopy %SDK%\node_modules\moment\min\moment-with-locales.js %SDK%\deps\moment\ /E /Y
-xcopy %SDK%\node_modules\rx-lite\rx.lite.js %SDK%\deps\rx-lite\ /E /Y
-xcopy %SDK%\node_modules\redux\dist\redux.min.js %SDK%\deps\redux\ /E /Y
-
 pushd %SDK%
 call grunt clean:css clean:js less
 call yarn run build
@@ -36,6 +30,5 @@ xcopy %SDK%\*.* deploy\bundle\model\Portal\SlxMobile\SourceFiles\argos-sdk /E /Y
 xcopy *.* %SDK%\deploy\temp /E /Y /exclude:build\bundleExcludes.txt
 xcopy %SDK%\deploy\temp\*.* deploy\bundle\model\Portal\SlxMobile\SourceFiles\products\argos-saleslogix /E /Y
 rmdir %SDK%\deploy\temp /S /Q
-rmdir %SDK%\deps /S /Q
 
 %SDK%\tools\bundler\Bundler.exe /ProjectPath:"%CD%\deploy\bundle\model" /BundleFileName:"%CD%\deploy\%BUNDLE_NAME%" /BundleMethod:All /ConfigFileName:"%CD%\build\bundle.config"
