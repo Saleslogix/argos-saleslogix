bf4868c32cb6ee2feee80b503f63f625dd5ffebd
Pukhraj Bal <pukhrajbal@gmail.com>
2017-06-09 10:38:34 -0700

    Lint

--- src/polyfills/nodelist.js
+++ src/polyfills/nodelist.js
@@ -2,9 +2,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
 export function nodelist() {
   if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = function (callback, argument) {
+    NodeList.prototype.forEach = function (callback, argument) { // eslint-disable-line
       argument = argument || window;
-      for (var i = 0; i < this.length; i++) {
+      for (let i = 0; i < this.length; i++) {
         callback.call(argument, this[i], i, this);
       }
     };
