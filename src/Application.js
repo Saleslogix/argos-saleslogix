9e9bd43bb779431c5debf396ee44b41b2183071e
Jason Best <jason.best@infor.com>
2019-02-08 11:02:17 -0500

    Binding timeout to failure.

--- src/Application.js
+++ src/Application.js
@@ -458,6 +458,7 @@ class Application extends SDKApplication {
       success: this.onAuthenticateUserSuccess.bind(this, credentials, options.success, options.scope),
       failure: this.onAuthenticateUserFailure.bind(this, options.failure, options.scope),
       aborted: this.onAuthenticateUserFailure.bind(this, options.failure, options.scope),
+      timeout: this.onAuthenticateUserFailure.bind(this, options.failure, options.scope),
     });
   }
   hasAccessTo(security) {
