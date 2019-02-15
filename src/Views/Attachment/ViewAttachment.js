bd904a7486b22a3770330cec6586393e9888e4fa
Jaren Rowan <jaren.rowan@infor.com>
2019-02-15 07:41:08 -0700

    INFORCRM-21842: Scroll error fix

--- src/Views/Attachment/ViewAttachment.js
+++ src/Views/Attachment/ViewAttachment.js
@@ -130,7 +130,7 @@ const __class = declare('crm.Views.Attachment.ViewAttachment', [Detail, _LegacyS
     '</svg>',
     '</button>',
     '</div>',
-    '<div style="overflow:scroll;">',
+    '<div style="overflow:auto;">',
     '<canvas id="pdfViewer">',
     '</canvas>',
     '</div>',
