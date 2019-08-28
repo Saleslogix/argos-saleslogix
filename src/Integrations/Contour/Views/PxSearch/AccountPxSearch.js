f0ef045d18646b31c532aae02945490bcde09e2a
Jaren Rowan <jaren.rowan@infor.com>
2019-08-28 08:08:59 -0700

    Searching for Accounts nearby now properly sorts based on distance when there are more records than the pagesize

--- src/Integrations/Contour/Views/PxSearch/AccountPxSearch.js
+++ src/Integrations/Contour/Views/PxSearch/AccountPxSearch.js
@@ -52,7 +52,7 @@ const __class = declare('crm.Integrations.Contour.Views.PxSearch.AccountPxSearch
     '<p class="micro-text">',
     '{%: $$.joinFields(" | ", [$.Type, $.SubType]) %}',
     '</p>',
-    '<p class="micro-text">{%: $.AccountManager && $.AccountManager.UserInfo ? $.AccountManager.UserInfo.UserName : "" %} | {%: $.Owner.OwnerDescription %}</p>',
+    '<p class="micro-text">{%: $.AccountManagerLF ? $.AccountManagerLF : "" %} | {%: $.OwnerDescription %}</p>',
     '{% if ($.MainPhone) { %}',
     '<p class="micro-text">',
     '{%: $$.phoneAbbreviationText %} <span class="hyperlink" data-action="callMain" data-key="{%: $.$key %}">{%: argos.Format.phone($.MainPhone) %}</span>', // TODO: Avoid global
@@ -122,13 +122,19 @@ const __class = declare('crm.Integrations.Contour.Views.PxSearch.AccountPxSearch
     const request = new Sage.SData.Client.SDataBaseRequest(this.getService());
     const pageSize = this.pageSize;
     const startIndex = this.feed && this.feed.$startIndex > 0 && this.feed.$itemsPerPage > 0 ? this.feed.$startIndex + this.feed.$itemsPerPage : 1;
-    request.uri.setPathSegment(0, 'slx');
-    request.uri.setPathSegment(1, 'dynamic');
+    request.uri.setPathSegment(0, '$app');
+    request.uri.setPathSegment(1, 'mashups');
     request.uri.setPathSegment(2, '-');
-    request.uri.setPathSegment(3, 'accounts');
+    request.uri.setPathSegment(3, 'mashups(\'GetAccountsByGeocode\')');
+    request.uri.setPathSegment(4, '$queries');
+    request.uri.setPathSegment(5, 'execute');
+    request.uri.setQueryArg('_resultName', 'GetAccountsByGeocodeMashup');
+    request.uri.setQueryArg('_Lat', this.lat);
+    request.uri.setQueryArg('_Lon', this.lon);
+    request.uri.setQueryArg('_Distance', this.maxDistance);
+    request.uri.setQueryArg('_AccountType', this.acctType ? this.acctType : 'All');
+    request.uri.setQueryArg('_SubType', 'All');
     request.uri.setQueryArg('format', 'JSON');
-    request.uri.setQueryArg('select', 'AccountName,Industry,Type,SubType,AccountManager/UserInfo/UserName,Address/GeocodeLatitude,Address/GeocodeLongitude,Owner/OwnerDescription,WebAddress,MainPhone,Fax');
-    request.uri.setQueryArg('where', `Type eq "${this.acctType ? this.acctType : 'Customer'}" and ${this._requestDistanceCalc()}`);
     request.uri.setStartIndex(startIndex);
     request.uri.setCount(pageSize);
     return request;
@@ -161,7 +167,7 @@ const __class = declare('crm.Integrations.Contour.Views.PxSearch.AccountPxSearch
     if (feedResources) {
       for (let i = 0; i < feed.$resources.length; i++) {
         const entry = feed.$resources[i];
-        entry.Distance = this.distanceCalc(entry.Address.GeocodeLatitude, entry.Address.GeocodeLongitude);
+        entry.Distance = this.distanceCalc(entry.GeocodeLatitude, entry.GeocodeLongitude);
       }
 
       // Sort by distance ASC
