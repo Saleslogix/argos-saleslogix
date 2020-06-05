75fb94da5bc9b65c9c039e6b38d70331d5a89b3d
Jason Best <skeeterbug@gmail.com>
2020-06-05 10:01:09 -0400

    Remove un-used reference to dojo/NodeList-Manipulate

--- src-out/Views/Help.js
+++ src-out/Views/Help.js
@@ -1,4 +1,4 @@
-define('crm/Views/Help', ['module', 'exports', 'dojo/_base/declare', 'argos/_DetailBase', 'argos/ErrorManager', 'argos/I18n', 'dojo/NodeList-manipulate'], function (module, exports, _declare, _DetailBase2, _ErrorManager, _I18n) {
+define('crm/Views/Help', ['module', 'exports', 'dojo/_base/declare', 'argos/_DetailBase', 'argos/ErrorManager', 'argos/I18n'], function (module, exports, _declare, _DetailBase2, _ErrorManager, _I18n) {
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
@@ -17,20 +17,22 @@ define('crm/Views/Help', ['module', 'exports', 'dojo/_base/declare', 'argos/_Det
     };
   }
 
-  var resource = (0, _I18n2.default)('help'); /* Copyright 2017 Infor
-                                               *
-                                               * Licensed under the Apache License, Version 2.0 (the "License");
-                                               * you may not use this file except in compliance with the License.
-                                               * You may obtain a copy of the License at
-                                               *
-                                               *    http://www.apache.org/licenses/LICENSE-2.0
-                                               *
-                                               * Unless required by applicable law or agreed to in writing, software
-                                               * distributed under the License is distributed on an "AS IS" BASIS,
-                                               * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-                                               * See the License for the specific language governing permissions and
-                                               * limitations under the License.
-                                               */
+  /* Copyright 2017 Infor
+   *
+   * Licensed under the Apache License, Version 2.0 (the "License");
+   * you may not use this file except in compliance with the License.
+   * You may obtain a copy of the License at
+   *
+   *    http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   */
+
+  var resource = (0, _I18n2.default)('help');
 
   var __class = (0, _declare2.default)('crm.Views.Help', [_DetailBase3.default], {
     // Templates
